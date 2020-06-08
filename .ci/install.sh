6364d839002902005c786f059f3b37500945c5a4
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-06-08 09:17:20 +0200

    use torch for CPU in CI

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+REQUIREMENTS_FILE=ci_torch_cpu_requirements.txt
+python3 gen_torch_cpu_requirements.py --file $REQUIREMENTS_FILE
+pip3 install -r $REQUIREMENTS_FILE
+
 pip3 install -r requirements-test.txt
 
 pip3 install --upgrade .  codecov
