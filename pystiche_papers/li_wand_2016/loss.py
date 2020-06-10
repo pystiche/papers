73466f6d98b1114e3a129b40f2dfba3fb8983c6b
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-06-10 08:31:04 +0200

    Port li_wand_2016 (#14)
    * port li_wand_2016
    
    * lint
    
    * Update replication/li_wand_2016/.gitignore
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * mypy
    
    * lint
    
    * Update mypy.ini
    
    * fix transforms
    
    * readd mirror transform
    
    * fix MD5 checksums
    
    * use target_transforms in custom MRFOperator
    
    * remove custom PerceptualLoss
    
    * cleanup
    
    Co-authored-by: julian <julian.bueltemeier@stud.hs-owl.de>
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

---
+++ pystiche_papers/li_wand_2016/loss.py
@@ -0,0 +1,253 @@
+from typing import Any, Dict, Iterable, Optional, Sequence, Tuple, Union
+
+import torch
+from torch.nn.functional import mse_loss
+
+import pystiche
+import pystiche.ops.functional as F
+from pystiche.enc import Encoder, MultiLayerEncoder
+from pystiche.image.transforms import Transform
+from pystiche.loss import PerceptualLoss
+from pystiche.misc import to_2d_arg
+from pystiche.ops import (
+    FeatureReconstructionOperator,
+    MRFOperator,
+    MultiLayerEncodingOperator,
+    TotalVariationOperator,
+)
+
+from .utils import li_wand_2016_multi_layer_encoder
+
+__all__ = [
+    "LiWand2016FeatureReconstructionOperator",
+    "li_wand_2016_content_loss",
+    "LiWand2016MRFOperator",
+    "li_wand_2016_style_loss",
+    "LiWand2016TotalVariationOperator",
+    "li_wand_2016_regularization",
+    "li_wand_2016_perceptual_loss",
+]
+
+
+class LiWand2016FeatureReconstructionOperator(FeatureReconstructionOperator):
+    def __init__(
+        self, encoder: Encoder, impl_params: bool = True, **mse_encoding_op_kwargs: Any,
+    ):
+        super().__init__(encoder, **mse_encoding_op_kwargs)
+
+        self.loss_reduction = "mean" if impl_params else "sum"
+
+    def calculate_score(
+        self,
+        input_repr: torch.Tensor,
+        target_repr: torch.Tensor,
+        ctx: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        return mse_loss(input_repr, target_repr, reduction=self.loss_reduction)
+
+
+def li_wand_2016_content_loss(
+    impl_params: bool = True,
+    multi_layer_encoder: Optional[MultiLayerEncoder] = None,
+    layer: str = "relu4_2",
+    score_weight: Optional[float] = None,
+) -> LiWand2016FeatureReconstructionOperator:
+    if multi_layer_encoder is None:
+        multi_layer_encoder = li_wand_2016_multi_layer_encoder()
+    encoder = multi_layer_encoder.extract_encoder(layer)
+
+    if score_weight is None:
+        score_weight = 2e1 if impl_params else 1e0
+
+    return LiWand2016FeatureReconstructionOperator(
+        encoder, impl_params=impl_params, score_weight=score_weight
+    )
+
+
+class NormalizeUnfoldGrad(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx: Any, input: torch.Tensor, dim: int, size: int, step: int) -> torch.Tensor:  # type: ignore[override]
+        ctx.needs_normalizing = step < size
+        if ctx.needs_normalizing:
+            normalizer = torch.zeros_like(input)
+            item = [slice(None) for _ in range(input.dim())]
+            for idx in range(0, normalizer.size()[dim] - size, step):
+                item[dim] = slice(idx, idx + size)
+                normalizer[item].add_(1.0)
+
+            # clamping to avoid zero division
+            ctx.save_for_backward(torch.clamp(normalizer, min=1.0))
+        return input
+
+    @staticmethod
+    def backward(ctx: Any, grad_output: torch.Tensor) -> Tuple[torch.Tensor, None, None, None]:  # type: ignore[override]
+        if ctx.needs_normalizing:
+            (normalizer,) = ctx.saved_tensors
+            grad_input = grad_output / normalizer
+        else:
+            grad_input = grad_output.clone()
+        return grad_input, None, None, None
+
+
+normalize_unfold_grad = NormalizeUnfoldGrad.apply  # type: ignore[attr-defined]
+
+
+def extract_normalized_patches2d(
+    input: torch.Tensor,
+    patch_size: Union[int, Sequence[int]],
+    stride: Union[int, Sequence[int]],
+) -> torch.Tensor:
+    patch_size = to_2d_arg(patch_size)
+    stride = to_2d_arg(stride)
+    for dim, size, step in zip(range(2, input.dim()), patch_size, stride):
+        input = normalize_unfold_grad(input, dim, size, step)
+    return pystiche.extract_patches2d(input, patch_size, stride)
+
+
+class LiWand2016MRFOperator(MRFOperator):
+    def __init__(
+        self,
+        encoder: Encoder,
+        patch_size: Union[int, Tuple[int, int]],
+        impl_params: bool = True,
+        **mrf_op_kwargs: Any,
+    ):
+
+        super().__init__(encoder, patch_size, **mrf_op_kwargs)
+
+        self.normalize_patches_grad = impl_params
+        self.loss_reduction = "sum"
+        self.score_correction_factor = 1.0 / 2.0 if impl_params else 1.0
+
+    def enc_to_repr(self, enc: torch.Tensor, is_guided: bool) -> torch.Tensor:
+        if self.normalize_patches_grad:
+            repr = extract_normalized_patches2d(enc, self.patch_size, self.stride)
+        else:
+            repr = pystiche.extract_patches2d(enc, self.patch_size, self.stride)
+        if not is_guided:
+            return repr
+
+        return self._guide_repr(repr)
+
+    def calculate_score(
+        self,
+        input_repr: torch.Tensor,
+        target_repr: torch.Tensor,
+        ctx: Optional[torch.Tensor],
+    ) -> torch.Tensor:
+        score = F.mrf_loss(input_repr, target_repr, reduction=self.loss_reduction)
+        return score * self.score_correction_factor
+
+
+def li_wand_2016_style_loss(
+    impl_params: bool = True,
+    multi_layer_encoder: Optional[MultiLayerEncoder] = None,
+    layers: Optional[Sequence[str]] = None,
+    layer_weights: Union[str, Sequence[float]] = "sum",
+    patch_size: Union[int, Tuple[int, int]] = 3,
+    stride: Optional[Union[int, Tuple[int, int]]] = None,
+    target_transforms: Optional[Iterable[Transform]] = None,
+    score_weight: Optional[float] = None,
+) -> MultiLayerEncodingOperator:
+    if multi_layer_encoder is None:
+        multi_layer_encoder = li_wand_2016_multi_layer_encoder()
+
+    if layers is None:
+        layers = ("relu3_1", "relu4_1")
+
+    if stride is None:
+        stride = 2 if impl_params else 1
+
+    if target_transforms is None:
+        num_scale_steps = 1 if impl_params else 3
+        scale_step_width = 5e-2
+        num_rotate_steps = 1 if impl_params else 2
+        rotate_step_width = 7.5
+        target_transforms = MRFOperator.scale_and_rotate_transforms(
+            num_scale_steps=num_scale_steps,
+            scale_step_width=scale_step_width,
+            num_rotate_steps=num_rotate_steps,
+            rotate_step_width=rotate_step_width,
+        )
+
+    def get_encoding_op(encoder: Encoder, layer_weight: float) -> LiWand2016MRFOperator:
+        return LiWand2016MRFOperator(
+            encoder,
+            patch_size,
+            impl_params=impl_params,
+            stride=stride,
+            target_transforms=target_transforms,
+            score_weight=layer_weight,
+        )
+
+    if score_weight is None:
+        score_weight = 1e-4 if impl_params else 1e0
+
+    return MultiLayerEncodingOperator(
+        multi_layer_encoder,
+        layers,
+        get_encoding_op,
+        layer_weights=layer_weights,
+        score_weight=score_weight,
+    )
+
+
+class LiWand2016TotalVariationOperator(TotalVariationOperator):
+    def __init__(self, impl_params: bool = True, **total_variation_op_kwargs: Any):
+        super().__init__(**total_variation_op_kwargs)
+
+        self.loss_reduction = "sum"
+        self.score_correction_factor = 1.0 / 2.0 if impl_params else 1.0
+
+    def calculate_score(self, input_repr: torch.Tensor) -> torch.Tensor:
+        score = F.total_variation_loss(
+            input_repr, exponent=self.exponent, reduction=self.loss_reduction
+        )
+        return score * self.score_correction_factor
+
+
+def li_wand_2016_regularization(
+    impl_params: bool = True, exponent: float = 2.0, score_weight: float = 1e-3,
+) -> LiWand2016TotalVariationOperator:
+    return LiWand2016TotalVariationOperator(
+        impl_params=impl_params, exponent=exponent, score_weight=score_weight
+    )
+
+
+def li_wand_2016_perceptual_loss(
+    impl_params: bool = True,
+    multi_layer_encoder: Optional[MultiLayerEncoder] = None,
+    content_loss_kwargs: Optional[Dict[str, Any]] = None,
+    style_loss_kwargs: Optional[Dict[str, Any]] = None,
+    regularization_kwargs: Optional[Dict[str, Any]] = None,
+) -> PerceptualLoss:
+    if multi_layer_encoder is None:
+        multi_layer_encoder = li_wand_2016_multi_layer_encoder()
+
+    if content_loss_kwargs is None:
+        content_loss_kwargs = {}
+    content_loss = li_wand_2016_content_loss(
+        impl_params=impl_params,
+        multi_layer_encoder=multi_layer_encoder,
+        **content_loss_kwargs,
+    )
+
+    if style_loss_kwargs is None:
+        style_loss_kwargs = {}
+    style_loss = li_wand_2016_style_loss(
+        impl_params=impl_params,
+        multi_layer_encoder=multi_layer_encoder,
+        **style_loss_kwargs,
+    )
+
+    if regularization_kwargs is None:
+        regularization_kwargs = {}
+    regularization = li_wand_2016_regularization(
+        impl_params=impl_params, **regularization_kwargs
+    )
+
+    return PerceptualLoss(
+        content_loss,
+        style_loss,  # type: ignore[arg-type]
+        regularization=regularization,
+    )
---
+++ replication/li_wand_2016/main.py
@@ -0,0 +1,83 @@
+import os
+from argparse import Namespace
+from os import path
+
+from pystiche.image import write_image
+from pystiche.misc import get_device
+from pystiche.optim import OptimLogger
+from pystiche_papers.li_wand_2016 import li_wand_2016_images, li_wand_2016_nst
+from pystiche_papers.utils import abort_if_cuda_memory_exausts
+
+
+@abort_if_cuda_memory_exausts
+def figure_6(args):
+    images = li_wand_2016_images()
+    images.download(args.image_source_dir)
+    positions = ("top", "bottom")
+    image_pairs = (
+        (images["blue_bottle"], images["self-portrait"]),
+        (images["s"], images["composition_viii"]),
+    )
+
+    for position, image_pair in zip(positions, image_pairs):
+        content_image = image_pair[0].read(device=args.device)
+        style_image = image_pair[1].read(device=args.device)
+
+        params = "implementation" if args.impl_params else "paper"
+        header = (
+            f"Replicating the {position} half of figure 6 " f"with {params} parameters"
+        )
+        with args.logger.environment(header):
+            output_image = li_wand_2016_nst(
+                content_image,
+                style_image,
+                impl_params=args.impl_params,
+                quiet=args.quiet,
+                logger=args.logger,
+            )
+
+            output_file = path.join(args.image_results_dir, f"fig_6__{position}.jpg")
+            args.logger.sep_message(f"Saving result to {output_file}", bottom_sep=False)
+            write_image(output_image, output_file)
+
+
+def parse_input():
+    # TODO: write CLI
+    image_source_dir = None
+    image_results_dir = None
+    device = None
+    impl_params = True
+    quiet = False
+
+    def process_dir(dir):
+        dir = path.abspath(path.expanduser(dir))
+        os.makedirs(dir, exist_ok=True)
+        return dir
+
+    here = path.dirname(__file__)
+
+    if image_source_dir is None:
+        image_source_dir = path.join(here, "images", "source")
+    image_source_dir = process_dir(image_source_dir)
+
+    if image_results_dir is None:
+        image_results_dir = path.join(here, "images", "results")
+    image_results_dir = process_dir(image_results_dir)
+
+    device = get_device(device)
+    logger = OptimLogger()
+
+    return Namespace(
+        image_source_dir=image_source_dir,
+        image_results_dir=image_results_dir,
+        device=device,
+        impl_params=impl_params,
+        logger=logger,
+        quiet=quiet,
+    )
+
+
+if __name__ == "__main__":
+    args = parse_input()
+
+    figure_6(args)
