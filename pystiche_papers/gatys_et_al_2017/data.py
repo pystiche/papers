9b0c2a29410655357f4fbd886e71fd2a3a62943d
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-06-10 11:12:53 +0200

    add guides

--- pystiche_papers/gatys_et_al_2017/data.py
+++ pystiche_papers/gatys_et_al_2017/data.py
@@ -3,6 +3,8 @@ from pystiche.data import (
     DownloadableImage,
     DownloadableImageCollection,
     ExpiredCopyrightLicense,
+    NoLicense,
+    PublicDomainLicense,
 )
 
 __all__ = ["gatys_et_al_2017_images"]
@@ -14,13 +16,51 @@ def gatys_et_al_2017_images() -> DownloadableImageCollection:
             "https://associateddesigns.com/sites/default/files/plan_images/main/craftsman_house_plan_tillamook_30-519-picart.jpg",
             title="House Concept Tillamook",
             date="2014",
+            license=NoLicense(),
             md5="5629bf7b24a7c98db2580ec2a8d784e9",
+            guides=DownloadableImageCollection(
+                {
+                    "building": DownloadableImage(
+                        "https://download.pystiche.org/images/house/building.png",
+                        file="building.png",
+                        author="Philip Meier",
+                        license=PublicDomainLicense(),
+                        md5="af6f7a11bdfb674cded10aa0c83021ad",
+                    ),
+                    "sky": DownloadableImage(
+                        "https://download.pystiche.org/images/house/sky.png",
+                        file="sky.png",
+                        author="Philip Meier",
+                        license=PublicDomainLicense(),
+                        md5="5070c703dae2cf62009e7d64ba839b1b",
+                    ),
+                }
+            ),
         ),
         "watertown": DownloadableImage(
             "https://ae01.alicdn.com/img/pb/136/085/095/1095085136_084.jpg",
             title="Watertown",
             author="Shop602835 Store",
+            license=NoLicense(),
             md5="4cc98a503da5ce6eab0649b09fd3cf77",
+            guides=DownloadableImageCollection(
+                {
+                    "building": DownloadableImage(
+                        "https://download.pystiche.org/images/watertown/building.png",
+                        file="building.png",
+                        author="Philip Meier",
+                        license=PublicDomainLicense(),
+                        md5="cca6f4f2877dec1ea422fc23c644b672",
+                    ),
+                    "sky": DownloadableImage(
+                        "https://download.pystiche.org/images/watertown/sky.png",
+                        file="sky.png",
+                        author="Philip Meier",
+                        license=PublicDomainLicense(),
+                        md5="4fdfc4ac4479dff3b6a1cf71d724f758",
+                    ),
+                }
+            ),
         ),
         "wheat_field": DownloadableImage(
             "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Wheat-Field-with-Cypresses-%281889%29-Vincent-van-Gogh-Met.jpg/1920px-Wheat-Field-with-Cypresses-%281889%29-Vincent-van-Gogh-Met.jpg",
@@ -29,6 +69,24 @@ def gatys_et_al_2017_images() -> DownloadableImageCollection:
             date="1889",
             license=ExpiredCopyrightLicense(1890),
             md5="bfd085d7e800459c8ffb44fa404f73c3",
+            guides=DownloadableImageCollection(
+                {
+                    "foreground": DownloadableImage(
+                        "https://download.pystiche.org/images/wheat_field/foreground.png",
+                        file="foreground.png",
+                        author="Philip Meier",
+                        license=PublicDomainLicense(),
+                        md5="696d73f7a5f4ea0b436ac1bbfb57cb37",
+                    ),
+                    "sky": DownloadableImage(
+                        "https://download.pystiche.org/images/wheat_field/sky.png",
+                        file="sky.png",
+                        author="Philip Meier",
+                        license=PublicDomainLicense(),
+                        md5="1102f4c57033c48be7d2142c8d6a3a29",
+                    ),
+                }
+            ),
         ),
         "schultenhof": DownloadableImage(
             "https://upload.wikimedia.org/wikipedia/commons/8/82/Schultenhof_Mettingen_Bauerngarten_8.jpg",
