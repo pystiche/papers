536fc51d4d28fa684ad50ed03a1e4a97b080654c
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-06-16 09:47:14 +0200

    Gatys et al 2017 (#18)
    * lint
    
    * mypy
    
    * mypy main
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * add guides
    
    * add test for images
    
    * Solve requested changes
    
    * Fix lint
    
    * Add gitignore
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

---
+++ pystiche_papers/gatys_et_al_2017/utils.py
@@ -0,0 +1,30 @@
+import torch
+from torch import optim
+
+from pystiche.enc import MultiLayerEncoder, vgg19_multi_layer_encoder
+from pystiche.image.transforms import CaffePostprocessing, CaffePreprocessing
+
+__all__ = [
+    "gatys_et_al_2017_preprocessor",
+    "gatys_et_al_2017_postprocessor",
+    "gatys_et_al_2017_multi_layer_encoder",
+    "gatys_et_al_2017_optimizer",
+]
+
+
+def gatys_et_al_2017_preprocessor() -> CaffePreprocessing:
+    return CaffePreprocessing()
+
+
+def gatys_et_al_2017_postprocessor() -> CaffePostprocessing:
+    return CaffePostprocessing()
+
+
+def gatys_et_al_2017_multi_layer_encoder() -> MultiLayerEncoder:
+    return vgg19_multi_layer_encoder(
+        weights="caffe", internal_preprocessing=False, allow_inplace=True
+    )
+
+
+def gatys_et_al_2017_optimizer(input_image: torch.Tensor) -> optim.LBFGS:  # type: ignore[name-defined]
+    return optim.LBFGS([input_image.requires_grad_(True)], lr=1.0, max_iter=1)  # type: ignore[attr-defined]
