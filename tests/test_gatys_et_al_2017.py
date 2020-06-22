ce4cbf5a652dc57fe788a3dfe8dcd00f2c0949e7
Philip Meier <github.pmeier@posteo.de>
2020-06-22 17:00:33 +0200

    fix test names (#32)

--- tests/test_gatys_et_al_2017.py
+++ tests/test_gatys_et_al_2017.py
@@ -5,7 +5,7 @@ from pystiche_papers import gatys_et_al_2017 as paper
 from .asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
 
-def test_ulyanov_et_al_2016_images_smoke(subtests):
+def test_gatys_et_al_2017_images_smoke(subtests):
     for name, image in paper.gatys_et_al_2017_images():
         with subtests.test(name=name):
             assert_image_is_downloadable(image)
--- tests/test_johnson_alahi_li_2016.py
+++ tests/test_johnson_alahi_li_2016.py
@@ -5,7 +5,7 @@ from pystiche_papers import johnson_alahi_li_2016 as paper
 from .asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
 
-def test_ulyanov_et_al_2016_images_smoke(subtests):
+def test_johnson_alahi_li_2016_smoke(subtests):
     for name, image in paper.johnson_alahi_li_2016_images():
         with subtests.test(name=name):
             assert_image_is_downloadable(image)
@@ -13,7 +13,7 @@ def test_ulyanov_et_al_2016_images_smoke(subtests):
 
 @pytest.mark.large_download
 @pytest.mark.slow
-def test_gatys_et_al_2017_images(subtests):
+def test_johnson_alahi_li_2016_images(subtests):
     for name, image in paper.johnson_alahi_li_2016_images():
         with subtests.test(name=name):
             assert_image_downloads_correctly(image)
