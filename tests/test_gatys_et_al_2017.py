dc6d15a19b7a6e2fc5a0d9666ec42def0b20c40c
jbueltemeier <jubue@gmx.net>
2020-06-23 09:39:10 +0200

    Correction of test

--- tests/test_gatys_et_al_2017.py
+++ tests/test_gatys_et_al_2017.py
@@ -1,5 +1,6 @@
 import pytest
-import torch
+
+from pystiche.image.transforms import CaffePreprocessing
 
 from pystiche_papers import gatys_et_al_2017 as paper
 
@@ -22,8 +23,5 @@ def test_gatys_et_al_2017_images(subtests):
 
 def test_gatys_et_al_2017_preprocessor():
     preprocessor = utils.gatys_et_al_2017_preprocessor()
-    input = torch.tensor([[[1.485]], [[2.458]], [[3.408]]])
-    actual = preprocessor(input)
-    desired = [3 * 255.0, 2 * 255.0, 1 * 255.0]
 
-    assert torch.flatten(actual).tolist() == desired
+    assert isinstance(preprocessor, CaffePreprocessing)
