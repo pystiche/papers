287219b7a57e0646ce9bfe434f6c95706ad09211
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-06-23 14:24:16 +0200

    Add tests for gatys_et_al_2017 utils (#35)
    * Add tests for gatys_et_al_2017 utils
    
    * Update tests/test_gatys_et_al_2017.py
    
    * Correct tests failure
    
    * re-add functionality after rebase
    
    * expand optimizer test
    
    * remove duplicated tests
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

--- tests/gatys_et_al_2017/test_utils.py
+++ tests/gatys_et_al_2017/test_utils.py
@@ -1,6 +1,41 @@
-from pystiche.image.transforms import CaffePreprocessing
+import pytest
+
+from torch import optim
+
+from pystiche.enc import VGGMultiLayerEncoder
+from pystiche.image.transforms import CaffePostprocessing, CaffePreprocessing
 from pystiche_papers.gatys_et_al_2017 import utils
 
 
 def test_gatys_et_al_2017_preprocessor():
     assert isinstance(utils.gatys_et_al_2017_preprocessor(), CaffePreprocessing)
+
+
+def test_gatys_et_al_2017_postprocessor():
+    assert isinstance(utils.gatys_et_al_2017_postprocessor(), CaffePostprocessing)
+
+
+@pytest.mark.large_download
+@pytest.mark.slow
+def test_gatys_et_al_2017_multi_layer_encoder():
+    assert isinstance(
+        utils.gatys_et_al_2017_multi_layer_encoder(), VGGMultiLayerEncoder
+    )
+
+
+def test_gatys_et_al_2017_optimizer(subtests, input_image):
+    params = input_image
+    optimizer = utils.gatys_et_al_2017_optimizer(params)
+
+    assert isinstance(optimizer, optim.LBFGS)
+    assert len(optimizer.param_groups) == 1
+
+    param_group = optimizer.param_groups[0]
+
+    with subtests.test(msg="optimization params"):
+        assert len(param_group["params"]) == 1
+        assert param_group["params"][0] is params
+
+    with subtests.test(msg="optimizer properties"):
+        assert pytest.approx(param_group["lr"]) == 1.0
+        assert param_group["max_iter"] == 1
