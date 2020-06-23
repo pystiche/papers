6fbc21b70b0b16321572dc7572d40f9326fafffc
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-06-23 14:17:07 +0200

    remove duplicated tests

--- tests/gatys_et_al_2017/test_data.py
+++ tests/gatys_et_al_2017/test_data.py
@@ -1,11 +1,6 @@
 import pytest
 
-from torch import optim
-
-from pystiche.enc.models.vgg import VGGMultiLayerEncoder
-from pystiche.image.transforms import CaffePostprocessing, CaffePreprocessing
 from pystiche_papers import gatys_et_al_2017 as paper
-from pystiche_papers.gatys_et_al_2017 import utils
 
 from .._asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
@@ -22,27 +17,3 @@ def test_gatys_et_al_2017_images(subtests):
     for name, image in paper.gatys_et_al_2017_images():
         with subtests.test(name=name):
             assert_image_downloads_correctly(image)
-
-
-def test_gatys_et_al_2017_preprocessor():
-    assert isinstance(utils.gatys_et_al_2017_preprocessor(), CaffePreprocessing)
-
-
-def test_gatys_et_al_2017_postprocessor():
-    assert isinstance(utils.gatys_et_al_2017_postprocessor(), CaffePostprocessing)
-
-
-@pytest.mark.large_download
-@pytest.mark.slow
-def test_gatys_et_al_2017_multi_layer_encoder():
-    assert isinstance(
-        utils.gatys_et_al_2017_multi_layer_encoder(), VGGMultiLayerEncoder
-    )
-
-
-def test_gatys_et_al_2017_optimizer(input_image):
-    optimizer = utils.gatys_et_al_2017_optimizer(input_image)
-    assert isinstance(optimizer, optim.LBFGS)
-    for param_group in optimizer.param_groups:
-        assert param_group["lr"] == 1.0
-        assert param_group["max_iter"] == 1
