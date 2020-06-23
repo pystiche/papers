023e3bebd594d244f74c68b35b275ca2e070c051
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-06-23 10:18:20 +0200

    Add tests gatys_et_al_2017 preprocessor (#33)
    * Add tests gatys_et_al_2017 preprocessor
    
    * Correction of test
    
    * Update tests/test_gatys_et_al_2017.py
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * Clean up
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

--- tests/test_gatys_et_al_2017.py
+++ tests/test_gatys_et_al_2017.py
@@ -1,6 +1,8 @@
 import pytest
 
+from pystiche.image.transforms import CaffePreprocessing
 from pystiche_papers import gatys_et_al_2017 as paper
+from pystiche_papers.gatys_et_al_2017 import utils
 
 from .asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
@@ -17,3 +19,7 @@ def test_gatys_et_al_2017_images(subtests):
     for name, image in paper.gatys_et_al_2017_images():
         with subtests.test(name=name):
             assert_image_downloads_correctly(image)
+
+
+def test_gatys_et_al_2017_preprocessor():
+    assert isinstance(utils.gatys_et_al_2017_preprocessor(), CaffePreprocessing)
