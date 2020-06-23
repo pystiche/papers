dce737ad9e6727cf52a3f9a3d769d2158f79e13a
jbueltemeier <jubue@gmx.net>
2020-06-23 10:31:37 +0200

    Add tests for gatys_et_al_2017 utils

--- tests/gatys_et_al_2017/test_data.py
+++ tests/gatys_et_al_2017/test_data.py
@@ -1,6 +1,9 @@
 import pytest
 
-from pystiche.image.transforms import CaffePreprocessing
+from torch import optim
+
+from pystiche.enc.models.vgg import VGGMultiLayerEncoder
+from pystiche.image.transforms import CaffePostprocessing, CaffePreprocessing
 from pystiche_papers import gatys_et_al_2017 as paper
 from pystiche_papers.gatys_et_al_2017 import utils
 
@@ -23,3 +26,20 @@ def test_gatys_et_al_2017_images(subtests):
 
 def test_gatys_et_al_2017_preprocessor():
     assert isinstance(utils.gatys_et_al_2017_preprocessor(), CaffePreprocessing)
+
+
+def test_gatys_et_al_2017_postprocessor():
+    assert isinstance(utils.gatys_et_al_2017_postprocessor(), CaffePostprocessing)
+
+
+def test_gatys_et_al_2017_multi_layer_encoder():
+    assert isinstance(
+        utils.gatys_et_al_2017_multi_layer_encoder(), VGGMultiLayerEncoder
+    )
+
+
+def test_gatys_et_al_2017_optimizer(input_image):
+    optimizer = utils.gatys_et_al_2017_optimizer(input_image)
+    assert isinstance(optimizer, optim.LBFGS)
+    assert optimizer.lr == 1.0
+    assert optimizer.max_iter == 1
