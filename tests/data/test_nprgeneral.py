7e9e16d142dc729f7700b0f90dae2ab0ee59e9d0
Philip Meier <github.pmeier@posteo.de>
2020-06-24 09:12:34 +0200

    add smoke test for NPRgeneralLicense (#38)

--- tests/data/test_nprgeneral.py
+++ tests/data/test_nprgeneral.py
@@ -5,6 +5,11 @@ from pystiche_papers import data
 from .._asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
 
+def test_NPRgeneralLicense_smoke():
+    _, image = next(iter(data.NPRgeneral()))
+    assert isinstance(repr(image.license), str)
+
+
 def test_NPRgeneral_smoke(subtests):
     for name, image in data.NPRgeneral():
         with subtests.test(name=name):
