11f3e46c0c8d9ea5bc404db0f1df6ec76e679638
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-06-24 11:43:23 +0200

    add tests for batch_up_image

--- tests/utils/test_misc.py
+++ tests/utils/test_misc.py
@@ -1,10 +1,55 @@
-import torch
+import pytest
 
+import torch
+from torch.utils.data import BatchSampler, DataLoader, SequentialSampler
+
+from pystiche.image import extract_batch_size, make_single_image
 from pystiche_papers import utils
 
 
-def test_batch_up_image():
-    pass
+def test_batch_up_image(input_image):
+    batch_size = 3
+
+    batched_up_image = utils.batch_up_image(input_image, batch_size)
+    assert extract_batch_size(batched_up_image) == batch_size
+
+
+def test_batch_up_image_with_single_image(input_image):
+    batch_size = 3
+
+    batched_up_image = utils.batch_up_image(make_single_image(input_image), batch_size)
+    assert extract_batch_size(batched_up_image) == batch_size
+
+
+def test_batch_up_image_with_batched_image(multi_input_image):
+    with pytest.raises(RuntimeError):
+        utils.batch_up_image(multi_input_image)
+
+
+def test_batch_up_image_missing_arg(input_image):
+    with pytest.raises(RuntimeError):
+        utils.batch_up_image(input_image)
+
+
+def test_batch_up_image_loader(input_image):
+    batch_size = 3
+    dataset = ()
+    loader = DataLoader(dataset, batch_size=batch_size)
+
+    batched_up_image = utils.batch_up_image(input_image, loader=loader)
+    assert extract_batch_size(batched_up_image) == batch_size
+
+
+def test_batch_up_image_loader_with_batch_sampler(input_image):
+    batch_size = 3
+    dataset = ()
+    batch_sampler = BatchSampler(
+        SequentialSampler(dataset), batch_size, drop_last=False
+    )
+    loader = DataLoader(dataset, batch_sampler=batch_sampler)
+
+    batched_up_image = utils.batch_up_image(input_image, loader=loader)
+    assert extract_batch_size(batched_up_image) == batch_size
 
 
 def test_make_reproducible(subtests):
