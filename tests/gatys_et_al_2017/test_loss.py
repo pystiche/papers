2060f96f322a579717539a7674e567f6ba2864cc
jbueltemeier <jubue@gmx.net>
2020-07-20 14:12:18 +0200

    Add test for gatys_et_al_2017 content_loss

--- tests/gatys_et_al_2017/test_loss.py
+++ tests/gatys_et_al_2017/test_loss.py
@@ -1,22 +1,15 @@
-from torch.nn.functional import mse_loss
+import pytest
 
+from pystiche.ops import FeatureReconstructionOperator
 from pystiche_papers.gatys_et_al_2017 import loss
 
 
-def test_gatys_et_al_2017_content_loss(
-    multi_layer_encoder_with_layer, target_image, input_image
-):
-    multi_layer_encoder, layer = multi_layer_encoder_with_layer
-    encoder = multi_layer_encoder.extract_encoder(layer)
-    target_enc = encoder(target_image)
-    input_enc = encoder(input_image)
+def test_gatys_et_al_2017_content_loss(subtests):
+    content_loss = loss.gatys_et_al_2017_content_loss()
+    assert isinstance(content_loss, FeatureReconstructionOperator)
 
-    op = loss.gatys_et_al_2017_content_loss(
-        multi_layer_encoder=multi_layer_encoder, layer=layer,
-    )
-    op.set_target_image(target_image)
-    actual = op(input_image)
+    with subtests.test("layer"):
+        assert content_loss.encoder.layer == "relu4_2"
 
-    desired = mse_loss(input_enc, target_enc)
-
-    assert (actual - desired).abs().max().item() < 1e-6
+    with subtests.test("score_weight"):
+        assert content_loss.score_weight == pytest.approx(1e0)
