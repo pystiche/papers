15f60a388ebf44c149825d3a997eef56da6f8607
jbueltemeier <jubue@gmx.net>
2020-07-20 14:13:11 +0200

    Add tests for gatys_et_al_2017 style_loss

--- tests/gatys_et_al_2017/test_loss.py
+++ tests/gatys_et_al_2017/test_loss.py
@@ -1,5 +1,9 @@
 import pytest
 
+import pytorch_testing_utils as ptu
+from torch.nn.functional import mse_loss
+
+from pystiche import gram_matrix, ops
 from pystiche.ops import FeatureReconstructionOperator
 from pystiche_papers.gatys_et_al_2017 import loss
 
@@ -13,3 +17,63 @@ def test_gatys_et_al_2017_content_loss(subtests):
 
     with subtests.test("score_weight"):
         assert content_loss.score_weight == pytest.approx(1e0)
+
+
+def test_GatysEtAl2017StyleLoss(
+    subtests, multi_layer_encoder_with_layer, target_image, input_image
+):
+
+    multi_layer_encoder, layer = multi_layer_encoder_with_layer
+    encoder = multi_layer_encoder.extract_encoder(layer)
+    target_repr = gram_matrix(encoder(target_image), normalize=True)
+    input_repr = gram_matrix(encoder(input_image), normalize=True)
+
+    configs = ((True, 1.0), (False, 1.0 / 4.0))
+    for impl_params, score_correction_factor in configs:
+        with subtests.test(impl_params=impl_params):
+            op = loss.GatysEtAl2017StyleLoss(
+                multi_layer_encoder,
+                (layer,),
+                lambda encoder, layer_weight: ops.GramOperator(
+                    encoder, score_weight=layer_weight
+                ),
+                impl_params=impl_params,
+                layer_weights="sum",
+            )
+
+            op.set_target_image(target_image)
+            actual = op(input_image)
+
+            score = mse_loss(input_repr, target_repr,)
+            desired = score * score_correction_factor
+
+            assert actual == ptu.approx(desired)
+
+
+def test_gatys_et_al_2017_style_loss(subtests):
+
+    style_loss = loss.gatys_et_al_2017_style_loss()
+    assert isinstance(style_loss, ops.MultiLayerEncodingOperator)
+
+    with subtests.test("encoding_ops"):
+        assert all(isinstance(op, ops.GramOperator) for op in style_loss.operators())
+
+    layers, layer_weights = zip(
+        *[(op.encoder.layer, op.score_weight) for op in style_loss.operators()]
+    )
+    with subtests.test("layers"):
+        assert set(layers) == {"relu1_1", "relu2_1", "relu3_1", "relu4_1", "relu5_1"}
+
+    with subtests.test("layer_weights"):
+        layers, nums_channels = zip(
+            ("relu1_1", 64),
+            ("relu2_1", 128),
+            ("relu3_1", 256),
+            ("relu4_1", 512),
+            ("relu5_1", 512),
+        )
+        desired = tuple(1.0 / num_channels ** 2.0 for num_channels in nums_channels)
+        assert layer_weights == pytest.approx(desired)
+
+    with subtests.test("score_weight"):
+        assert style_loss.score_weight == pytest.approx(1e3)
