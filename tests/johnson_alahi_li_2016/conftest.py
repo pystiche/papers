b2a8f24fa22e96ab3b925495e54f3ea527e101ae
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-21 14:26:09 +0200

    add tests for johnson_alahi_li_2016.loss

---
+++ tests/johnson_alahi_li_2016/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+
+from pystiche_papers.gatys_ecker_bethge_2015.utils import (
+    gatys_ecker_bethge_2015_multi_layer_encoder,
+)
+
+
+@pytest.fixture(scope="package")
+def vgg_load_weights_mock(package_mocker):
+    return package_mocker.patch(
+        "pystiche.enc.models.vgg.VGGMultiLayerEncoder._load_weights"
+    )
+
+
+@pytest.fixture(scope="package", autouse=True)
+def multi_layer_encoder_mock(package_mocker, vgg_load_weights_mock):
+    multi_layer_encoder = gatys_ecker_bethge_2015_multi_layer_encoder()
+
+    def new(impl_params=None):
+        multi_layer_encoder.empty_storage()
+        return multi_layer_encoder
+
+    return package_mocker.patch(
+        "pystiche_papers.johnson_alahi_li_2016.loss.johnson_alahi_li_2016_multi_layer_encoder",
+        new,
+    )
