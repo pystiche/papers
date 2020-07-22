7f84e6ed63a0b7c1b028878715c49a053760e4d2
jbueltemeier <jubue@gmx.net>
2020-07-22 16:18:44 +0200

    mock OctaveImagePyramid

--- tests/li_wand_2016/test_pyramid.py
+++ tests/li_wand_2016/test_pyramid.py
@@ -4,23 +4,35 @@ from pystiche.pyramid import OctaveImagePyramid
 from pystiche_papers.li_wand_2016 import pyramid
 
 
-def test_li_wand_2016_image_pyramid(subtests):
+def test_li_wand_2016_image_pyramid(subtests, mocker):
 
     configs = ((True, 100, 3), (False, 200, None))
     for (impl_params, num_steps, num_levels,) in configs:
         with subtests.test(impl_params=impl_params):
+
             if num_levels is None:
                 num_levels = int(np.floor(np.log2(6))) + 1
-            li_wand_pyramid = pyramid.li_wand_2016_image_pyramid(
-                impl_params=impl_params, num_steps=num_steps, num_levels=num_levels,
-            )
-            assert isinstance(li_wand_pyramid, OctaveImagePyramid)
+            mock = mocker.patch("pystiche.pyramid.pyramid.OctaveImagePyramid")
+            pyramid.li_wand_2016_image_pyramid(impl_params=impl_params)
 
-            with subtests.test("num_steps"):
-                pyramid_num_steps = tuple(
-                    level.num_steps for level in li_wand_pyramid._levels
-                )
-                assert pyramid_num_steps == (num_steps,) * len(li_wand_pyramid._levels)
+            for call in mock.call_args_list:
+                args, kwargs = call
+                pyramid_max_edge_size, pyramid_num_steps = args
+                pyramid_num_levels = kwargs["num_levels"]
+                pyramid_min_edge_size = kwargs["min_edge_size"]
+                pyramid_edge = kwargs["edge"]
 
-            with subtests.test("num_levels"):
-                assert len(li_wand_pyramid._levels) == num_levels
+                with subtests.test("max_edge_size"):
+                    assert pyramid_max_edge_size == 384
+
+                with subtests.test("num_steps"):
+                    assert pyramid_num_steps == num_steps
+
+                with subtests.test("num_levels"):
+                    assert pyramid_num_levels == num_levels
+
+                with subtests.test("min_edge_size"):
+                    assert pyramid_min_edge_size == 64
+
+                with subtests.test("edge"):
+                    assert pyramid_edge == "long"
