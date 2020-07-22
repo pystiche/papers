8370870ffc0611e964314a8a8e1a2469464f50b5
jbueltemeier <jubue@gmx.net>
2020-07-22 07:58:12 +0200

    Add guided_nst test

--- tests/gatys_et_al_2017/test_core.py
+++ tests/gatys_et_al_2017/test_core.py
@@ -39,3 +39,47 @@ def test_gatys_et_al_2017_nst_smoke(subtests, mocker, content_image, style_image
 
     with subtests.test("postprocessor"):
         assert isinstance(postprocessor, type(utils.gatys_et_al_2017_postprocessor()))
+
+
+def test_gatys_et_al_2017_guided_nst_smoke(
+    subtests, mocker, content_image, style_image, content_guides, style_guides
+):
+    mock = mocker.patch(
+        "pystiche_papers.gatys_et_al_2017.core.default_image_pyramid_optim_loop"
+    )
+    styles = {}
+    for key in style_guides.keys():
+        styles[key] = (style_image, style_guides[key])
+
+    paper.gatys_et_al_2017_guided_nst(content_image, content_guides, styles)
+
+    args, kwargs = mock.call_args
+    input_image, criterion, pyramid = args
+    get_optimizer = kwargs["get_optimizer"]
+    preprocessor = kwargs["preprocessor"]
+    postprocessor = kwargs["postprocessor"]
+
+    with subtests.test("input_image"):
+        ptu.assert_allclose(input_image, pyramid[-1].resize_image(content_image))
+
+    with subtests.test("criterion"):
+        assert isinstance(
+            criterion,
+            type(paper.gatys_et_al_2017_guided_perceptual_loss(content_guides.keys())),
+        )
+
+    with subtests.test("pyramid"):
+        assert isinstance(pyramid, type(paper.gatys_et_al_2017_image_pyramid()))
+
+    with subtests.test("optimizer"):
+        assert is_callable(get_optimizer)
+        optimizer = get_optimizer(input_image)
+        assert isinstance(
+            optimizer, type(utils.gatys_et_al_2017_optimizer(input_image))
+        )
+
+    with subtests.test("preprocessor"):
+        assert isinstance(preprocessor, type(utils.gatys_et_al_2017_preprocessor()))
+
+    with subtests.test("postprocessor"):
+        assert isinstance(postprocessor, type(utils.gatys_et_al_2017_postprocessor()))
