cc3b63000051d60779acf52364f06320db42cdd9
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-07-23 09:09:09 +0200

    Add test li_wand_2016 pyramid (#80)
    * Add test pyramid
    
    * mock OctaveImagePyramid
    
    * lint
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * lint
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

---
+++ tests/li_wand_2016/test_pyramid.py
@@ -0,0 +1,36 @@
+from pystiche_papers.li_wand_2016 import pyramid
+
+
+def test_li_wand_2016_image_pyramid(subtests, mocker):
+
+    configs = ((True, 100, 3), (False, 200, None))
+    for (impl_params, num_steps, num_levels,) in configs:
+        with subtests.test(impl_params=impl_params):
+
+            mock = mocker.patch("pystiche.pyramid.pyramid.OctaveImagePyramid")
+            pyramid.li_wand_2016_image_pyramid(impl_params=impl_params)
+
+            for call in mock.call_args_list:
+                args, kwargs = call
+                pyramid_max_edge_size, pyramid_num_steps = args
+                pyramid_num_levels = kwargs["num_levels"]
+                pyramid_min_edge_size = kwargs["min_edge_size"]
+                pyramid_edge = kwargs["edge"]
+
+                with subtests.test("max_edge_size"):
+                    assert pyramid_max_edge_size == 384
+
+                with subtests.test("num_steps"):
+                    assert pyramid_num_steps == num_steps
+
+                with subtests.test("num_levels"):
+                    if isinstance(num_levels, int):
+                        assert pyramid_num_levels == num_levels
+                    else:
+                        assert isinstance(pyramid_num_levels, int)
+
+                with subtests.test("min_edge_size"):
+                    assert pyramid_min_edge_size == 64
+
+                with subtests.test("edge"):
+                    assert pyramid_edge == "long"
