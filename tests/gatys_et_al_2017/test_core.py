d356c7a10cf92e10a4c6e6b3a9b6c959e6163d50
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-23 09:15:06 +0200

    adapt patch / mock infrastructure from #79

--- tests/gatys_et_al_2017/test_core.py
+++ tests/gatys_et_al_2017/test_core.py
@@ -1,3 +1,5 @@
+import unittest.mock
+
 import pytest
 
 import pytorch_testing_utils as ptu
@@ -7,14 +9,91 @@ from pystiche_papers.gatys_et_al_2017 import utils
 from tests._utils import is_callable
 
 
-def test_gatys_et_al_2017_nst_smoke(subtests, mocker, content_image, style_image):
-    mock = mocker.patch(
-        "pystiche_papers.gatys_et_al_2017.core.default_image_pyramid_optim_loop"
+def make_patch_target(name, prefix=True):
+    return ".".join(
+        (
+            "pystiche_papers",
+            "gatys_et_al_2017",
+            "core",
+            ("gatys_et_al_2017_" if prefix else "") + name,
+        )
     )
+
+
+def attach_method_mock(mock, method, **attrs):
+    if "name" not in attrs:
+        attrs["name"] = f"{mock.name}.{method}()"
+
+    method_mock = unittest.mock.Mock(**attrs)
+    mock.attach_mock(method_mock, method)
+
+
+@pytest.fixture
+def make_nn_module_mock(mocker):
+    def make_nn_module_mock_(name=None, identity=False, **kwargs):
+        attrs = {}
+        if name is not None:
+            attrs["name"] = name
+        if identity:
+            attrs["side_effect"] = lambda x: x
+        attrs.update(kwargs)
+
+        mock = mocker.Mock(**attrs)
+
+        for method in ("eval", "to", "train"):
+            attach_method_mock(mock, method, return_value=mock)
+
+        return mock
+
+    return make_nn_module_mock_
+
+
+@pytest.fixture
+def patcher(mocker):
+    def patcher_(name, prefix=True, **kwargs):
+        return mocker.patch(make_patch_target(name, prefix=prefix), **kwargs)
+
+    return patcher_
+
+
+@pytest.fixture
+def preprocessor_mocks(make_nn_module_mock, patcher):
+    mock = make_nn_module_mock(identity=True)
+    patch = patcher("preprocessor", return_value=mock)
+    return patch, mock
+
+
+@pytest.fixture
+def postprocessor_mocks(make_nn_module_mock, patcher):
+    mock = make_nn_module_mock(identity=True)
+    patch = patcher("postprocessor", return_value=mock)
+    return patch, mock
+
+
+@pytest.fixture
+def perceptual_loss_mocks(make_nn_module_mock, patcher):
+    mock = make_nn_module_mock()
+    attach_method_mock(mock, "set_content_image", return_value=None)
+    attach_method_mock(mock, "set_content_guide", return_value=None)
+    attach_method_mock(mock, "set_style_image", return_value=None)
+    attach_method_mock(mock, "set_style_guide", return_value=None)
+    patch = patcher("perceptual_loss", return_value=mock)
+    return patch, mock
+
+
+@pytest.fixture(autouse=True)
+def default_image_pyramid_optim_loop_patch(patcher):
+    return patcher("default_image_pyramid_optim_loop", prefix=False)
+
+
+def test_gatys_et_al_2017_nst_smoke(
+    subtests, default_image_pyramid_optim_loop_patch, content_image, style_image
+):
+    patch = default_image_pyramid_optim_loop_patch
 
     paper.gatys_et_al_2017_nst(content_image, style_image)
 
-    args, kwargs = mock.call_args
+    args, kwargs = patch.call_args
     input_image, criterion, pyramid = args
     get_optimizer = kwargs["get_optimizer"]
     preprocessor = kwargs["preprocessor"]
@@ -44,17 +123,19 @@ def test_gatys_et_al_2017_nst_smoke(subtests, mocker, content_image, style_image
 
 
 def test_gatys_et_al_2017_guided_nst_smoke(
-    subtests, mocker, content_image, content_guides, style_images_and_guides
+    subtests,
+    default_image_pyramid_optim_loop_patch,
+    content_image,
+    content_guides,
+    style_images_and_guides,
 ):
-    mock = mocker.patch(
-        "pystiche_papers.gatys_et_al_2017.core.default_image_pyramid_optim_loop"
-    )
+    patch = default_image_pyramid_optim_loop_patch
 
     paper.gatys_et_al_2017_guided_nst(
         content_image, content_guides, style_images_and_guides
     )
 
-    args, kwargs = mock.call_args
+    args, kwargs = patch.call_args
     input_image, criterion, pyramid = args
     get_optimizer = kwargs["get_optimizer"]
     preprocessor = kwargs["preprocessor"]
