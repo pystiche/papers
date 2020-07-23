059cd65dbfde667d83d58a08502316d7c25debdb
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-23 08:43:32 +0200

    add test for region mismatch

--- tests/gatys_et_al_2017/test_core.py
+++ tests/gatys_et_al_2017/test_core.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pytorch_testing_utils as ptu
 
 import pystiche_papers.gatys_et_al_2017 as paper
@@ -85,3 +87,17 @@ def test_gatys_et_al_2017_guided_nst_smoke(
 
     with subtests.test("postprocessor"):
         assert isinstance(postprocessor, type(utils.gatys_et_al_2017_postprocessor()))
+
+
+def test_test_gatys_et_al_2017_guided_nst_regions_mismatch(
+    content_image, content_guides, style_image, style_guides
+):
+    style_images_and_guides = {
+        region: (style_image, guide)
+        for region, guide in tuple(style_guides.items())[:-1]
+    }
+
+    with pytest.raises(RuntimeError):
+        paper.gatys_et_al_2017_guided_nst(
+            content_image, content_guides, style_images_and_guides
+        )
