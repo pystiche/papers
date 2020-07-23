7e461732ea0443ac6939776ba97f335b723b2bb6
jbueltemeier <jubue@gmx.net>
2020-07-23 10:01:07 +0200

    Add get_conv test

---
+++ tests/johnson_alahi_li_2016/test_modules.py
@@ -0,0 +1,53 @@
+import itertools
+
+from pystiche_papers.johnson_alahi_li_2016 import modules
+
+
+def test_get_conv(subtests, mocker):
+    in_channels = out_channels = 3
+    kernel_size = 3
+    stride = 1
+    padding = (None, 1, (1, 1))
+    upsample = (True, False)
+    configs = itertools.product(padding, upsample)
+    for padding, upsample in configs:
+        with subtests.test(upsample):
+            mock = mocker.patch(
+                "torch.nn.Conv2d" if upsample else "torch.nn.ConvTranspose2d"
+            )
+
+            modules.get_conv(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride=stride,
+                padding=padding,
+                upsample=upsample,
+            )
+
+            for call in mock.call_args_list:
+                args, kwargs = call
+                conv_in_channels, conv_out_channels, conv_kernel_size, = args
+                conv_stride = kwargs["stride"]
+                conv_padding = kwargs["padding"]
+
+                with subtests.test("in_channels"):
+                    assert conv_in_channels == in_channels
+
+                with subtests.test("out_channels"):
+                    assert conv_out_channels == out_channels
+
+                with subtests.test("kernel_size"):
+                    assert conv_kernel_size == kernel_size
+
+                with subtests.test("stride"):
+                    assert conv_stride == stride
+
+                with subtests.test("padding"):
+                    assert conv_padding == (1, 1)
+
+                if upsample:
+                    conv_output_padding = kwargs["output_padding"]
+
+                    with subtests.test("output_padding"):
+                        assert conv_output_padding == (1, 1)
