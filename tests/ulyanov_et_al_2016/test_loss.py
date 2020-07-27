e9ace0461fb1ed7ec0fd7ea7be3fc8061c690ced
jbueltemeier <jubue@gmx.net>
2020-07-27 08:41:54 +0200

    Add tests content_loss

--- tests/ulyanov_et_al_2016/test_loss.py
+++ tests/ulyanov_et_al_2016/test_loss.py
@@ -1,3 +1,6 @@
+
+import pytest
+
 import pytorch_testing_utils as ptu
 from torch.nn.functional import mse_loss
 
@@ -5,28 +8,48 @@ from pystiche.image import extract_batch_size
 from pystiche_papers.ulyanov_et_al_2016 import loss
 
 
-def test_ulyanov_et_al_2016_content_loss(
-    subtests, multi_layer_encoder_with_layer, batch_image
+def test_UlyanovEtAl2016FeatureReconstructionOperator(
+    subtests, multi_layer_encoder_with_layer, target_image, input_image
 ):
     multi_layer_encoder, layer = multi_layer_encoder_with_layer
     encoder = multi_layer_encoder.extract_encoder(layer)
-    target_image = batch_image
-    input_image = batch_image.flip((0,))
     target_enc = encoder(target_image)
     input_enc = encoder(input_image)
-    batch_size = extract_batch_size(batch_image)
 
-    configs = ((True, batch_size), (False, 1.0))
-    for impl_params, extra_batch_size_mean in configs:
-        op = loss.ulyanov_et_al_2016_content_loss(
+    for impl_params in (True, False):
+        with subtests.test(impl_params=impl_params):
+            op = loss.UlyanovEtAl2016FeatureReconstructionOperator(
+                encoder, impl_params=impl_params,
+            )
+            op.set_target_image(target_image)
+            actual = op(input_image)
+
+            score = mse_loss(input_enc, target_enc)
+
+            desired = score / extract_batch_size(input_enc) if impl_params else score
+
+            assert actual == ptu.approx(desired)
+
+
+def test_ulyanov_et_al_2016_content_loss(subtests):
+    configs = (
+        (True, True, 1e0),
+        (True, False, 6e-1),
+        (False, True, 1e0),
+        (False, False, 1e0),
+    )
+    for impl_params, instance_norm, score_weight in configs:
+        content_loss = loss.ulyanov_et_al_2016_content_loss(
             impl_params=impl_params,
-            multi_layer_encoder=multi_layer_encoder,
-            layer=layer,
+            instance_norm=instance_norm,
+            score_weight=score_weight,
         )
-        op.set_target_image(target_image)
-        actual = op(input_image)
+        assert isinstance(
+            content_loss, loss.UlyanovEtAl2016FeatureReconstructionOperator
+        )
 
-        score = mse_loss(input_enc, target_enc)
-        desired = score / extra_batch_size_mean
+        with subtests.test("layer"):
+            assert content_loss.encoder.layer == "relu4_2"
 
-        assert actual == ptu.approx(desired)
+        with subtests.test("score_weight"):
+            assert content_loss.score_weight == pytest.approx(score_weight)
