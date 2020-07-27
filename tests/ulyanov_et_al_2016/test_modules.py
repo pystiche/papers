aeba1358b9ce3f01416c8d88da6c6ad919e38b20
jbueltemeier <jubue@gmx.net>
2020-07-27 18:58:10 +0200

    Add tests resize_modules

--- tests/ulyanov_et_al_2016/test_modules.py
+++ tests/ulyanov_et_al_2016/test_modules.py
@@ -7,7 +7,7 @@ import pytorch_testing_utils as ptu
 import torch
 from torch import nn
 
-from pystiche.image.utils import  extract_num_channels
+from pystiche.image.utils import extract_num_channels
 from pystiche_papers.ulyanov_et_al_2016 import modules
 
 
@@ -51,6 +51,47 @@ def test_join_channelwise(subtests, input_image, style_image):
         ptu.assert_allclose(join_image[:, :input_num_channels, :, :], input_image)
     with subtests.test("style_image"):
         ptu.assert_allclose(join_image[:, input_num_channels:, :, :], style_image)
+
+
+def test_UlyanovEtAl2016StylizationDownsample(subtests):
+    module = modules.UlyanovEtAl2016StylizationDownsample()
+    assert isinstance(module, nn.AvgPool2d)
+    with subtests.test("kernel_size"):
+        assert module.kernel_size == pytest.approx(2)
+    with subtests.test("stride"):
+        assert module.stride == pytest.approx(2)
+    with subtests.test("padding"):
+        assert module.padding == pytest.approx(0)
+
+
+def test_UlyanovEtAl2016TextureDownsample(mocker, input_image):
+    mock = mocker.patch(
+        "pystiche_papers.ulyanov_et_al_2016.modules.TextureNoiseParams.downsample"
+    )
+    module = modules.UlyanovEtAl2016TextureDownsample()
+    module(input_image)
+    mock.assert_called_once()
+
+
+def test_ulyanov_et_al_2016_downsample(subtests):
+    for stylization in (True, False):
+        with subtests.test(stylization=stylization):
+            module = modules.ulyanov_et_al_2016_downsample(stylization=stylization)
+            assert isinstance(
+                module,
+                modules.UlyanovEtAl2016StylizationDownsample
+                if stylization
+                else modules.UlyanovEtAl2016TextureDownsample,
+            )
+
+
+def test_ulyanov_et_al_2016_upsample(subtests):
+    module = modules.ulyanov_et_al_2016_upsample()
+    assert isinstance(module, nn.Upsample)
+    with subtests.test("scale_factor"):
+        assert module.scale_factor == pytest.approx(2.0)
+    with subtests.test("mode"):
+        assert module.mode == "nearest"
 
 
 def test_get_norm_module(subtests):
