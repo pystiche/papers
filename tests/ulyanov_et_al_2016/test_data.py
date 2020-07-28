43700ca5d23dff194d5b2c8463564074e8f3300f
jbueltemeier <jubue@gmx.net>
2020-07-28 15:53:50 +0200

    Add tests batch_sampler, dataset, image_loader

--- tests/ulyanov_et_al_2016/test_data.py
+++ tests/ulyanov_et_al_2016/test_data.py
@@ -4,10 +4,13 @@ import pytest
 
 import pytorch_testing_utils as ptu
 import torch
+from torch.utils.data import DataLoader
 
+from pystiche.data import ImageFolderDataset
 from pystiche.image import transforms
 from pystiche.image.transforms import CenterCrop, ValidRandomCrop
 from pystiche.image.transforms.functional import grayscale_to_fakegrayscale, resize
+from pystiche_papers.data.utils import FiniteCycleBatchSampler
 from pystiche_papers.ulyanov_et_al_2016 import data
 from pystiche_papers.utils import make_reproducible
 
@@ -81,3 +84,60 @@ def test_ulyanov_et_al_2016_images(subtests):
     for name, image in data.ulyanov_et_al_2016_images():
         with subtests.test(name=name):
             assert_image_downloads_correctly(image)
+
+
+def test_ulyanov_et_al_2016_dataset(subtests, mocker):
+    mocker.patch(
+        "pystiche.data.datasets.ImageFolderDataset._collect_image_files",
+        return_value=[],
+    )
+    dataset = data.ulyanov_et_al_2016_dataset("root")
+
+    assert isinstance(dataset, ImageFolderDataset)
+
+    with subtests.test("transform"):
+        assert isinstance(
+            dataset.transform, type(data.ulyanov_et_al_2016_content_transform())
+        )
+
+
+def test_ulyanov_et_al_2016_batch_sampler(subtests):
+    data_source = ()
+    for impl_params, instance_norm in itertools.product((True, False), (True, False)):
+        with subtests.test(impl_params=impl_params, instance_norm=instance_norm):
+            batch_sampler = data.ulyanov_et_al_2016_batch_sampler(
+                data_source, impl_params=impl_params, instance_norm=instance_norm
+            )
+
+            assert isinstance(batch_sampler, FiniteCycleBatchSampler)
+
+            with subtests.test("num_batches"):
+                assert (
+                    batch_sampler.num_batches == 200
+                    if not impl_params
+                    else 2000
+                    if instance_norm
+                    else 300
+                )
+
+            with subtests.test("num_batches"):
+                assert (
+                    batch_sampler.batch_size == 16
+                    if not impl_params
+                    else 1
+                    if instance_norm
+                    else 4
+                )
+
+
+def test_ulyanov_et_al_2016_image_loader(subtests):
+    dataset = ()
+    image_loader = data.ulyanov_et_al_2016_image_loader(dataset)
+
+    assert isinstance(image_loader, DataLoader)
+
+    with subtests.test("batch_sampler"):
+        assert isinstance(
+            image_loader.batch_sampler,
+            type(data.ulyanov_et_al_2016_batch_sampler(dataset)),
+        )
