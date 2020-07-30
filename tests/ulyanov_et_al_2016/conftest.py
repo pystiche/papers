feadd376d73ca3d518cddd310b0eed8eff2ba6e8
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-30 11:24:24 +0200

    try mocking trim of multi_layer_encoder

--- tests/ulyanov_et_al_2016/conftest.py
+++ tests/ulyanov_et_al_2016/conftest.py
@@ -16,6 +16,11 @@ def vgg_load_weights_mock(package_mocker):
 def multi_layer_encoder_mock(package_mocker, vgg_load_weights_mock):
     multi_layer_encoder = ulyanov_et_al_2016_multi_layer_encoder()
 
+    def trim_mock(layers):
+        pass
+
+    multi_layer_encoder.trim = trim_mock
+
     def new(impl_params=None):
         multi_layer_encoder.empty_storage()
         return multi_layer_encoder
