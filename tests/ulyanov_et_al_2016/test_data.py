272b87e982b04c3a565e1eecd5fb29069da4f856
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-07-31 10:22:10 +0200

    Tests ulyanov 2016 data (#94)
    * Add tests transform
    
    * Add tests batch_sampler, dataset, image_loader
    
    * Add new test grayscale_image
    
    * lint

--- tests/ulyanov_et_al_2016/test_data.py
+++ tests/ulyanov_et_al_2016/test_data.py
@@ -1,13 +1,108 @@
+import itertools
+
 import pytest
 
-from pystiche_papers import ulyanov_et_al_2016 as paper
+import pytorch_testing_utils as ptu
+import torch
+from torch.utils.data import DataLoader
+
+from pystiche.data import ImageFolderDataset
+from pystiche.image import transforms
+from pystiche.image.transforms import CenterCrop, ValidRandomCrop
+from pystiche.image.transforms.functional import grayscale_to_fakegrayscale, resize
+from pystiche_papers.data.utils import FiniteCycleBatchSampler
+from pystiche_papers.ulyanov_et_al_2016 import data
+from pystiche_papers.utils import make_reproducible
 
 from .._asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
 
+def test_ulyanov_et_al_2016_content_transform(subtests):
+    make_reproducible()
+    image = torch.rand(1, 3, 32, 32)
+    edge_size = 16
+
+    for impl_params, instance_norm in itertools.product((True, False), (True, False)):
+        with subtests.test(impl_params=impl_params, instance_norm=instance_norm):
+            make_reproducible()
+            content_transform = data.ulyanov_et_al_2016_content_transform(
+                edge_size=edge_size,
+                impl_params=impl_params,
+                instance_norm=instance_norm,
+            )
+            actual = content_transform(image)
+
+            if impl_params:
+                if instance_norm:
+                    make_reproducible()
+                    transform = ValidRandomCrop(edge_size)
+                    desired = transform(image)
+                else:
+                    desired = resize(image, edge_size)
+            else:
+                transform = CenterCrop(edge_size)
+                desired = transform(image)
+
+            ptu.assert_allclose(actual, desired)
+
+
+def test_ulyanov_et_al_2016_content_transform_grayscale_image(subtests):
+    make_reproducible()
+    image = torch.rand(1, 1, 32, 32)
+    edge_size = 16
+
+    for impl_params, instance_norm in itertools.product((True, False), (True, False)):
+        with subtests.test(impl_params=impl_params, instance_norm=instance_norm):
+            make_reproducible()
+            content_transform = data.ulyanov_et_al_2016_content_transform(
+                edge_size=edge_size,
+                impl_params=impl_params,
+                instance_norm=instance_norm,
+            )
+            actual = content_transform(image)
+
+            if impl_params:
+                if instance_norm:
+                    make_reproducible()
+                    transform = ValidRandomCrop(edge_size)
+                    transform_image = transform(image)
+                else:
+                    transform_image = resize(image, edge_size)
+            else:
+                transform = CenterCrop(edge_size)
+                transform_image = transform(image)
+
+            desired = grayscale_to_fakegrayscale(transform_image)
+
+            ptu.assert_allclose(actual, desired)
+
+
+def test_ulyanov_et_al_2016_style_transform(subtests):
+    for impl_params, instance_norm in itertools.product((True, False), (True, False)):
+        with subtests.test(impl_params=impl_params, instance_norm=instance_norm):
+            style_transform = data.ulyanov_et_al_2016_style_transform(
+                impl_params=impl_params, instance_norm=instance_norm
+            )
+
+            assert isinstance(style_transform, transforms.ComposedTransform)
+
+            with subtests.test("edge_size"):
+                assert tuple(style_transform.children())[0].size == 256
+
+            with subtests.test("edge"):
+                assert tuple(style_transform.children())[0].edge == "long"
+
+            with subtests.test("interpolation_mode"):
+                assert (
+                    tuple(style_transform.children())[0].interpolation_mode == "bicubic"
+                    if impl_params and instance_norm
+                    else "bilinear"
+                )
+
+
 @pytest.mark.slow
 def test_ulyanov_et_al_2016_images_smoke(subtests):
-    for name, image in paper.ulyanov_et_al_2016_images():
+    for name, image in data.ulyanov_et_al_2016_images():
         with subtests.test(name=name):
             assert_image_is_downloadable(image)
 
@@ -15,6 +110,57 @@ def test_ulyanov_et_al_2016_images_smoke(subtests):
 @pytest.mark.large_download
 @pytest.mark.slow
 def test_ulyanov_et_al_2016_images(subtests):
-    for name, image in paper.ulyanov_et_al_2016_images():
+    for name, image in data.ulyanov_et_al_2016_images():
         with subtests.test(name=name):
             assert_image_downloads_correctly(image)
+
+
+def test_ulyanov_et_al_2016_dataset(subtests, mocker):
+    mocker.patch(
+        "pystiche.data.datasets.ImageFolderDataset._collect_image_files",
+        return_value=[],
+    )
+    dataset = data.ulyanov_et_al_2016_dataset("root")
+
+    assert isinstance(dataset, ImageFolderDataset)
+
+    with subtests.test("transform"):
+        assert isinstance(
+            dataset.transform, type(data.ulyanov_et_al_2016_content_transform())
+        )
+
+
+def test_ulyanov_et_al_2016_batch_sampler(subtests):
+    data_source = ()
+    configs = (
+        (True, True, 2000, 1),
+        (True, False, 300, 4),
+        (False, True, 200, 16),
+        (False, False, 200, 16),
+    )
+    for impl_params, instance_norm, num_batches, batch_size in configs:
+        with subtests.test(impl_params=impl_params, instance_norm=instance_norm):
+            batch_sampler = data.ulyanov_et_al_2016_batch_sampler(
+                data_source, impl_params=impl_params, instance_norm=instance_norm
+            )
+
+            assert isinstance(batch_sampler, FiniteCycleBatchSampler)
+
+            with subtests.test("num_batches"):
+                assert batch_sampler.num_batches == num_batches
+
+            with subtests.test("num_size"):
+                assert batch_sampler.batch_size == batch_size
+
+
+def test_ulyanov_et_al_2016_image_loader(subtests):
+    dataset = ()
+    image_loader = data.ulyanov_et_al_2016_image_loader(dataset)
+
+    assert isinstance(image_loader, DataLoader)
+
+    with subtests.test("batch_sampler"):
+        assert isinstance(
+            image_loader.batch_sampler,
+            type(data.ulyanov_et_al_2016_batch_sampler(dataset)),
+        )
