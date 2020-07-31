1f5c7a930cf0892610a77c8b86d9e9408e027a86
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-31 14:09:40 +0200

    allow importing without installing

--- pystiche_papers/__init__.py
+++ pystiche_papers/__init__.py
@@ -1,1 +1,7 @@
-from ._version import version as __version__  # type: ignore[import]
+import warnings
+
+try:
+    from ._version import version as __version__  # type: ignore[import]
+except ImportError:
+    warnings.warn("version file not found")
+    __version__ = "UNKNOWN"
