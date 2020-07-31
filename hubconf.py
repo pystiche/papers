5859fc59102889a4c3f3bbe75931de5885531962
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-31 14:00:45 +0200

    enable PyTorch hub

---
+++ hubconf.py
@@ -0,0 +1,5 @@
+dependencies = ["torch>=1.6", "torchvision>=0.7", "pystiche"]
+
+from pystiche_papers.johnson_alahi_li_2016 import (
+    johnson_alahi_li_2016_transformer as johnson_alahi_li_2016_transformer,
+)
