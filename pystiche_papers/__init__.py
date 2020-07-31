6704fb07a7f2d02c282ec0f3be92a8dde3b0803e
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-07-31 16:35:56 +0200

    remove warning if version file is missing

--- pystiche_papers/__init__.py
+++ pystiche_papers/__init__.py
@@ -1,7 +1,4 @@
-import warnings
-
 try:
     from ._version import version as __version__  # type: ignore[import]
 except ImportError:
-    warnings.warn("version file not found")
     __version__ = "UNKNOWN"
