a33283a8b78c178c87800a604f27d6c63e909fad
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-08-03 12:41:03 +0200

    handle env vars in test config

--- tests/hub/conftest.py
+++ tests/hub/conftest.py
@@ -7,7 +7,17 @@ from torch import hub
 
 @pytest.fixture(scope="package")
 def github():
-    return os.getenv("PYSTICHE_HUB_GITHUB", default="pmeier/pystiche_papers:master")
+    if os.getenv("GITHUB_ACTIONS"):
+        owner_and_repo = os.getenv("GITHUB_REPOSITORY")
+
+        branch_or_tag = os.getenv("GITHUB_HEAD_REF")
+        is_pr = branch_or_tag is not None
+        if not is_pr:
+            branch_or_tag = os.getenv("GITHUB_REF").rsplit("/", 1)[1]
+
+        return f"{owner_and_repo}:{branch_or_tag}"
+    else:
+        return os.getenv("PYSTICHE_HUB_GITHUB", default="pmeier/pystiche_papers:master")
 
 
 @pytest.fixture(scope="package", autouse=True)
