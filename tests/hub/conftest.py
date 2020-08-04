9dc4484afc1f7e4717d89148a629a24751d3a766
Philip Meier <github.pmeier@posteo.de>
2020-08-04 12:20:36 +0200

    partially fix hub tests for PRs (#119)
    * use GITHUB_ACTOR
    
    * fix env var pass through

--- tests/hub/conftest.py
+++ tests/hub/conftest.py
@@ -8,14 +8,17 @@ from torch import hub
 @pytest.fixture(scope="package")
 def github():
     if os.getenv("GITHUB_ACTIONS"):
-        owner_and_repo = os.getenv("GITHUB_REPOSITORY")
+        # FIXME: this environment variable does not contain the owner of the repository
+        #  but rather the person who triggered the workflow.
+        #  See https://github.com/pmeier/pystiche_papers/issues/116 for details
+        owner = os.getenv("GITHUB_ACTOR")
 
         branch_or_tag = os.getenv("GITHUB_HEAD_REF")
         is_pr = bool(branch_or_tag)
         if not is_pr:
             branch_or_tag = os.getenv("GITHUB_REF").rsplit("/", 1)[1]
 
-        return f"{owner_and_repo}:{branch_or_tag}"
+        return f"{owner}/pystiche_papers:{branch_or_tag}"
     else:
         return os.getenv("PYSTICHE_HUB_GITHUB", default="pmeier/pystiche_papers:master")
 
