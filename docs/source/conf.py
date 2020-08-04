4b832feaaa246a2a6d53fb6ebffbf77e99e16366
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-08-04 14:51:29 +0200

    better type hints handling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,6 +67,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
