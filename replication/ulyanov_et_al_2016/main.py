a11046479ae1e899cdd8636412746ddfd7403b26
Philip Meier <github.pmeier@posteo.de>
2020-08-06 11:17:16 +0200

    add replication tests for ulyanov_et_al_2016 (#142)
    * add replication tests for ulyanov_et_al_2016
    
    * fix image keys

--- replication/ulyanov_et_al_2016/main.py
+++ replication/ulyanov_et_al_2016/main.py
@@ -2,100 +2,12 @@ import os
 from argparse import Namespace
 from os import path
 
-import torch
-
 import pystiche_papers.ulyanov_et_al_2016 as paper
-from pystiche import image, misc, optim
+from pystiche import misc, optim
 from pystiche_papers import utils
 
 
-def training_texture(args):
-    textures = (
-        "cezanne",
-        "bricks",
-        "pebble",
-        "pixels",
-        "peppers",
-    )
-
-    dataset = paper.dataset(
-        path.join(args.dataset_dir, "texture"),
-        impl_params=args.impl_params,
-        instance_norm=args.instance_norm,
-    )
-    image_loader = paper.image_loader(
-        dataset,
-        impl_params=args.impl_params,
-        instance_norm=args.instance_norm,
-        stylization=args.stylization,
-        pin_memory=str(args.device).startswith("cuda"),
-    )
-
-    images = paper.images()
-    images.download(args.image_source_dir)
-
-    for texture in textures:
-        texture_image = images[texture].read(device=args.device)
-
-        transformer = paper.training(
-            image_loader,
-            texture_image,
-            impl_params=args.impl_params,
-            instance_norm=args.instance_norm,
-            stylization=args.stylization,
-            quiet=args.quiet,
-            logger=args.logger,
-        )
-
-        model_name = f"ulyanov_et_al_2016__{texture}"
-        if args.impl_params:
-            model_name += "__impl_params"
-        if args.instance_norm:
-            model_name += "__instance_norm"
-        utils.save_state_dict(transformer, model_name, root=args.model_dir)
-
-        output_image = paper.texture_synthesis(
-            torch.empty((1, 1, 256, 256), device="cuda"),
-            transformer,
-            impl_params=args.impl_params,
-            instance_norm=args.instance_norm,
-        )
-
-        output_name = f"{texture}"
-        if args.impl_params:
-            output_name += "__impl_params"
-        if args.instance_norm:
-            output_name += "__instance_norm"
-        output_file = path.join(args.image_results_dir, "texture", f"{output_name}.jpg")
-        image.write_image(output_image, output_file)
-
-        output_image = paper.texture_synthesis(
-            torch.empty((1, 1, 512, 512), device="cuda"),
-            transformer,
-            impl_params=args.impl_params,
-            instance_norm=args.instance_norm,
-            stylization=args.stylization,
-        )
-
-        output_name = f"{texture}"
-        output_name += "512_"
-        if args.impl_params:
-            output_name += "__impl_params"
-        if args.instance_norm:
-            output_name += "__instance_norm"
-        output_file = path.join(args.image_results_dir, "texture", f"{output_name}.jpg")
-        image.write_image(output_image, output_file)
-
-
-def training_style(args):
-    contents = (
-        "karya",
-        "tiger",
-        "neckarfront",
-        "che_high",
-        "the_tower_of_babel" "bird",
-        "kitty",
-    )
+def training(args):
     styles = (
         "candy",
         "the_scream",
@@ -115,7 +27,6 @@ def training_style(args):
         dataset,
         impl_params=args.impl_params,
         instance_norm=args.instance_norm,
-        stylization=args.stylization,
         pin_memory=str(args.device).startswith("cuda"),
     )
 
@@ -130,7 +41,6 @@ def training_style(args):
             style_image,
             impl_params=args.impl_params,
             instance_norm=args.instance_norm,
-            stylization=args.stylization,
             quiet=args.quiet,
             logger=args.logger,
         )
@@ -142,36 +52,15 @@ def training_style(args):
             model_name += "__instance_norm"
         utils.save_state_dict(transformer, model_name, root=args.model_dir)
 
-        for content in contents:
-            content_image = images[content].read().to(args.device)
-            output_image = paper.stylization(
-                content_image,
-                transformer,
-                impl_params=args.impl_params,
-                instance_norm=args.instance_norm,
-            )
-
-            output_name = f"{style}_{content}"
-            if args.impl_params:
-                output_name += "__impl_params"
-            if args.instance_norm:
-                output_name += "__instance_norm"
-            output_file = path.join(
-                args.image_results_dir, "style", f"{output_name}.jpg"
-            )
-            image.write_image(output_image, output_file)
-
 
 def parse_input():
     # TODO: write CLI
     image_source_dir = None
-    image_results_dir = None
     dataset_dir = None
     model_dir = None
     device = None
     impl_params = True
     instance_norm = False
-    stylization = True
     quiet = False
 
     def process_dir(dir):
@@ -184,10 +73,6 @@ def parse_input():
     if image_source_dir is None:
         image_source_dir = path.join(here, "data", "images", "source")
     image_source_dir = process_dir(image_source_dir)
-
-    if image_results_dir is None:
-        image_results_dir = path.join(here, "data", "images", "results")
-    image_results_dir = process_dir(image_results_dir)
 
     if dataset_dir is None:
         dataset_dir = path.join(here, "data", "images", "dataset")
@@ -202,13 +87,11 @@ def parse_input():
 
     return Namespace(
         image_source_dir=image_source_dir,
-        image_results_dir=image_results_dir,
         dataset_dir=dataset_dir,
         model_dir=model_dir,
         device=device,
         impl_params=impl_params,
         instance_norm=instance_norm,
-        stylization=stylization,
         logger=logger,
         quiet=quiet,
     )
@@ -216,5 +99,4 @@ def parse_input():
 
 if __name__ == "__main__":
     args = parse_input()
-    training_texture(args)
-    training_style(args)
+    training(args)
