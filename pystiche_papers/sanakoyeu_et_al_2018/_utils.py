959828147d1a9d60275df8fb50bedbfb4bd025cf
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-08-20 10:16:32 +0200

    Add sanakoyeu et_al_2018 utils (#161)
    * Initial commit
    
    * Add reference paper
    
    * Move sanakoyeu utils
    
    * Add tests for utils
    
    * lint
    
    * Add requested changes
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * Remove test errors
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

---
+++ pystiche_papers/sanakoyeu_et_al_2018/_utils.py
@@ -0,0 +1,70 @@
+from typing import Iterator, Union
+
+import torch
+from torch import nn, optim
+from torch.optim.lr_scheduler import ExponentialLR
+from torch.optim.optimizer import Optimizer
+
+from pystiche.optim.meter import FloatMeter
+
+from ..data.utils import DelayedExponentialLR
+
+
+def optimizer(
+    params: Union[nn.Module, Iterator[torch.nn.parameter.Parameter]]
+) -> Optimizer:
+    r"""Optimizer from :cite:`SKL+2018`.
+
+    Args:
+        params: Parameter to be optimized.
+
+    """
+    if isinstance(params, nn.Module):
+        params = params.parameters()
+    return optim.Adam(params, lr=2e-4)
+
+
+def lr_scheduler(optimizer: Optimizer) -> ExponentialLR:
+    r"""Learning rate scheduler from :cite:`SKL+2018`.
+
+    Args:
+        optimizer: Wrapped optimizer.
+
+    """
+    return DelayedExponentialLR(optimizer, gamma=0.1, delay=2)
+
+
+class ExponentialMovingAverageMeter(FloatMeter):
+    def __init__(
+        self,
+        name: str,
+        init_val: float = 0.0,
+        smoothing_factor: float = 0.05,
+        fmt: str = "{:f}",
+    ) -> None:
+        super().__init__(name)
+        self.smoothing_factor = smoothing_factor
+        self.last_val = init_val
+        self.fmt = fmt
+
+    def calculate_val(self, val: Union[torch.Tensor, float]) -> float:
+        if isinstance(val, torch.Tensor):
+            val = val.item()
+        return (
+            self.last_val * (1.0 - self.smoothing_factor) + self.smoothing_factor * val
+        )
+
+    def update(self, new_val: Union[torch.Tensor, float]) -> None:  # type: ignore[override]
+        super().update(self.calculate_val(new_val))
+
+    @property
+    def global_avg(self) -> float:
+        return self.last_val
+
+    def __str__(self) -> str:
+        def format(val: float) -> str:
+            return self.fmt.format(val)
+
+        val = format(self.last_val)
+        info = f"{val}"
+        return f"{self.name} {info}"
---
+++ tests/unit/sanakoyeu_et_al_2018/test_utils.py
@@ -0,0 +1,85 @@
+import pytest
+
+import pytorch_testing_utils as ptu
+import torch
+from torch import nn, optim
+
+import pystiche_papers.sanakoyeu_et_al_2018 as paper
+
+
+def test_optimizer_modules(subtests):
+    transformer = nn.Conv2d(3, 3, 1)
+    params = tuple(transformer.parameters())
+
+    for optimizer_params in [transformer, transformer.parameters()]:
+        with subtests.test("parameters"):
+            optimizer = paper.optimizer(optimizer_params)
+
+            assert isinstance(optimizer, optim.Adam)
+            assert len(optimizer.param_groups) == 1
+
+            param_group = optimizer.param_groups[0]
+
+            with subtests.test(msg="optimization params"):
+                assert len(param_group["params"]) == len(params)
+                for actual, desired in zip(param_group["params"], params):
+                    assert actual is desired
+
+            with subtests.test(msg="optimizer properties"):
+                assert param_group["lr"] == ptu.approx(2e-4)
+
+
+def test_lr_scheduler():
+    transformer = nn.Conv2d(3, 3, 1)
+    optimizer = paper.optimizer(transformer)
+    lr_scheduler = paper.lr_scheduler(optimizer)
+
+    assert isinstance(lr_scheduler, paper.DelayedExponentialLR)
+
+
+def test_ExponentialMovingAverageMeter(subtests):
+    vals = [0.6, 0.7, 0.8]
+    init_val = 0.8
+    smoothing_factor = 0.05
+    name = "test_ema"
+    ema = paper.ExponentialMovingAverageMeter(name, init_val)
+
+    with subtests.test("name"):
+        assert ema.name == name
+
+    with subtests.test("global_avg"):
+        desired = init_val
+        for val in vals:
+            ema.update(val)
+            desired = desired * (1.0 - smoothing_factor) + smoothing_factor * val
+
+        current = ema.global_avg
+        assert current == ptu.approx(desired)
+
+
+def test_ExponentialMovingAverageMeter_update_with_tensor(subtests):
+    vals = [torch.tensor([0.6]), torch.tensor([0.7]), torch.tensor([0.8])]
+    init_val = 0.8
+    smoothing_factor = 0.05
+    name = "test_ema"
+    ema = paper.ExponentialMovingAverageMeter(name, init_val)
+
+    with subtests.test("init_val"):
+        assert ema.global_avg == pytest.approx(init_val)
+
+    with subtests.test("global_avg"):
+        desired = init_val
+        for val in vals:
+            ema.update(val)
+            desired = desired * (1.0 - smoothing_factor) + smoothing_factor * val
+
+        current = ema.global_avg
+        assert current == ptu.approx(desired)
+
+
+def test_ExponentialMovingAverageMeter_str_smoke():
+    init_val = 0.8
+    meter = paper.ExponentialMovingAverageMeter(
+        "test_exponential_moving_average_meter", init_val
+    )
+    assert isinstance(str(meter), str)
