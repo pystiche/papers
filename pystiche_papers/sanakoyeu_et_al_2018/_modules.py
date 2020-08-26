93c53d6ae3bd065d062934aa08a8dca38f80941d
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2020-08-26 09:50:03 +0200

    Sanakoyeu 2018 basic modules (#166)
    * Move basic modules and add doc
    
    * Add tests
    
    * Clean up and fix lint errors
    
    * clean up
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * rename function activation to get_activation
    
    * Add descriptive error message
    
    * Remove too detailed description
    
    * Add residualblock description
    
    * Change activation test
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>
    
    * Add suggestions from code review to all docs
    
    * rename deconvolution block
    
    * remove exceptions tests
    
    * Fix rename errors
    
    * Rename DeconvBlock to UpsampleConvBlock
    
    * Apply suggestions from code review
    
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

---
+++ pystiche_papers/sanakoyeu_et_al_2018/_modules.py
@@ -0,0 +1,186 @@
+from typing import List, Tuple, Union, cast
+
+import torch
+from torch import nn
+
+from pystiche.misc import verify_str_arg
+
+from ..utils import ResidualBlock, same_size_padding
+
+__all__ = [
+    "get_padding",
+    "get_activation",
+    "conv",
+    "ConvBlock",
+    "UpsampleConvBlock",
+    "residual_block",
+]
+
+
+def get_padding(
+    padding: str, kernel_size: Union[Tuple[int, int], int]
+) -> Union[Tuple[int, int], int]:
+    padding = verify_str_arg(padding, valid_args=["same", "valid"])
+    if padding == "same":
+        return cast(Tuple[int, int], same_size_padding(kernel_size))
+    else:  # padding == "valid"
+        return 0
+
+
+def get_activation(act: str = "relu", inplace: bool = True) -> nn.Module:
+    act = verify_str_arg(act, valid_args=["relu", "lrelu"])
+    if act == "relu":
+        return nn.ReLU(inplace=inplace)
+    else:  # act == "lrelu"
+        return nn.LeakyReLU(negative_slope=0.2, inplace=inplace)
+
+
+def conv(
+    in_channels: int,
+    out_channels: int,
+    kernel_size: Union[Tuple[int, int], int],
+    stride: Union[Tuple[int, int], int] = 2,
+    padding: str = "valid",
+) -> nn.Conv2d:
+    padding = get_padding(padding, kernel_size)
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size, stride=stride, padding=padding
+    )
+
+
+class ConvBlock(nn.Sequential):
+    r"""ConvBlock from :cite:`SKL+2018`.
+
+    This block comprises a convolution followed by a normalization and an optional
+    activation.
+
+    Args:
+        in_channels: Number of channels in the input.
+        out_channels: Number of channels produced by the convolution.
+        kernel_size: Size of the convolving kernel.
+        stride: Stride of the convolution. Defaults to ``1``.
+        padding: Padding of the input. It can be either ``"valid"`` for no padding or
+            ``"same"`` for padding to preserve the size. Defaults to ``"valid"``.
+        act: The activation is either ``"relu"`` for a :class:`~torch.nn.ReLU`,
+            ``"lrelu"`` for a :class:`~torch.nn.LeakyReLU` with ``slope=0.2`` or
+            ``None`` for no activation. Defaults to ``"relu"``.
+        inplace: If ``True`` perform the activation in-place.
+
+    The parameters ``kernel_size`` and ``stride`` can either be:
+    * a single :class:`int` – in which case the same value is used for the height and
+      width dimension
+    * a tuple of two :class:`int` s – in which case, the first int is used for the
+      vertical dimension, and the second int for the horizontal dimension
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[Tuple[int, int], int],
+        stride: Union[Tuple[int, int], int] = 1,
+        padding: str = "valid",
+        act: Union[str, None] = "relu",
+        inplace: bool = True,
+    ) -> None:
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        modules: List[nn.Module] = []
+        modules.append(
+            conv(in_channels, out_channels, kernel_size, stride=stride, padding=padding)
+        )
+
+        modules.append(nn.InstanceNorm2d(out_channels))
+
+        if act is not None:
+            modules.append(get_activation(act=act, inplace=inplace))
+
+        super().__init__(*modules)
+
+
+class UpsampleConvBlock(nn.Module):
+    r"""UpsampleConvBlock from :cite:`SKL+2018`.
+
+    This block upsamples the input followed by a :class:`ConvBlock`.
+
+    Args:
+        in_channels: Number of channels in the input.
+        out_channels: Number of channels produced by the convolution.
+        kernel_size: Size of the convolving kernel.
+        scale_factor: ``scale_factor`` of the interpolation. Defaults to ``2.0``.
+        padding: Padding of the input. It can be either ``"valid"`` for no padding or
+            ``"same"`` for padding to preserve the size. Defaults to ``"same"``.
+        act: The activation is either ``"relu"`` for a :class:`~torch.nn.ReLU`,
+            ``"lrelu"`` for a :class:`~torch.nn.LeakyReLU` with ``slope=0.2`` or
+            ``None`` for no activation. Defaults to ``"relu"``.
+        inplace: If ``True`` perform the activation in-place.
+
+    The parameters ``kernel_size`` and ``stride`` can either be:
+    * a single :class:`int` – in which case the same value is used for the height and
+      width dimension
+    * a tuple of two :class:`int` s – in which case, the first int is used for the
+      vertical dimension, and the second int for the horizontal dimension
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[Tuple[int, int], int],
+        scale_factor: Union[Tuple[float, float], float] = 2.0,
+        padding: str = "same",
+        act: Union[str, None] = "relu",
+        inplace: bool = True,
+    ) -> None:
+        super().__init__()
+        self.scale_factor = scale_factor
+        self.conv = ConvBlock(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=1,
+            padding=padding,
+            act=act,
+            inplace=inplace,
+        )
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        return cast(
+            torch.Tensor,
+            self.conv(
+                nn.functional.interpolate(
+                    input, scale_factor=self.scale_factor, mode="nearest"
+                )
+            ),
+        )
+
+
+def residual_block(channels: int) -> ResidualBlock:
+    r"""Residual block from :cite:`SKL+2018`.
+
+    This block comprises two
+    :class:`ConvBlock` without activation
+    but respective prior reflection padding to maintain the input size as a ``residual``
+    of a :class:`pystiche_papers.utils.modules.ResidualBlock`.
+
+    Args:
+        channels: Number of channels in the input.
+
+    """
+    in_channels = out_channels = channels
+    kernel_size = 3
+    padding = same_size_padding(kernel_size)
+
+    residual = nn.Sequential(
+        nn.ReflectionPad2d(padding),
+        ConvBlock(
+            in_channels, out_channels, kernel_size, stride=1, padding="valid", act=None,
+        ),
+        nn.ReflectionPad2d(padding),
+        ConvBlock(
+            in_channels, out_channels, kernel_size, stride=1, padding="valid", act=None,
+        ),
+    )
+
+    return ResidualBlock(residual)
---
+++ tests/unit/sanakoyeu_et_al_2018/test_modules.py
@@ -0,0 +1,116 @@
+import pytest
+
+import torch
+from torch import nn
+
+import pystiche_papers.sanakoyeu_et_al_2018 as paper
+from pystiche import misc
+from pystiche_papers.utils import ResidualBlock, same_size_padding
+
+
+def test_get_padding(subtests):
+    kernel_size = 3
+    for str_padding, desired in (
+        ("same", same_size_padding(kernel_size)),
+        ("valid", 0),
+    ):
+        with subtests.test(str_padding):
+            actual = paper.get_padding(str_padding, kernel_size)
+            assert actual == desired
+
+
+def test_get_activation(subtests):
+    for str_act, desired in (("relu", nn.ReLU), ("lrelu", nn.LeakyReLU)):
+        with subtests.test(str_act):
+            actual = paper.get_activation(str_act)
+
+            assert isinstance(actual, desired)
+
+            with subtests.test("inplace"):
+                assert actual.inplace
+
+            if isinstance(actual, nn.LeakyReLU):
+                with subtests.test("slope"):
+                    assert actual.negative_slope == pytest.approx(0.2)
+
+
+def test_conv(subtests):
+    in_channels = out_channels = 3
+    kernel_size = 3
+    stride = 2
+    padding = "valid"
+    conv = paper.conv(
+        in_channels, out_channels, kernel_size, stride=stride, padding=padding
+    )
+
+    assert isinstance(conv, nn.Conv2d)
+    with subtests.test("in_channels"):
+        assert conv.in_channels == in_channels
+    with subtests.test("out_channels"):
+        assert conv.out_channels == out_channels
+    with subtests.test("kernel_size"):
+        assert conv.kernel_size == misc.to_2d_arg(kernel_size)
+    with subtests.test("stride"):
+        assert conv.stride == misc.to_2d_arg(stride)
+    with subtests.test("padding"):
+        assert conv.padding == misc.to_2d_arg(0)
+
+
+def test_ConvBlock(subtests):
+    in_channels = out_channels = 3
+    kernel_size = 3
+    stride = 1
+
+    for act in ("relu", "lrelu", None):
+        with subtests.test(act):
+            conv_block = paper.ConvBlock(
+                in_channels, out_channels, kernel_size, stride=stride, act=act
+            )
+
+            assert isinstance(conv_block, paper.ConvBlock)
+
+            with subtests.test("modules"):
+                assert len(conv_block) == 3 if act is not None else 2
+                assert isinstance(conv_block[0], nn.Conv2d)
+                assert isinstance(conv_block[1], nn.InstanceNorm2d)
+                if act is not None:
+                    assert isinstance(
+                        conv_block[2], nn.ReLU if act == "relu" else nn.LeakyReLU
+                    )
+
+
+def test_UpsampleConvBlock(subtests, input_image):
+    in_channels = out_channels = 3
+    kernel_size = 3
+    scale_factor = 2
+
+    upsample_conv_block = paper.UpsampleConvBlock(
+        in_channels, out_channels, kernel_size, scale_factor=scale_factor
+    )
+
+    output_image = upsample_conv_block(input_image)
+    assert isinstance(output_image, torch.Tensor)
+
+    with subtests.test("conv_module"):
+        assert isinstance(upsample_conv_block.conv, paper.ConvBlock)
+
+    with subtests.test("interpolate"):
+        desired_image = nn.functional.interpolate(
+            input_image, scale_factor=scale_factor, mode="nearest"
+        )
+        assert output_image.size() == desired_image.size()
+
+
+def test_residual_block(subtests, input_image):
+    channels = 3
+    residual_block = paper.residual_block(channels)
+
+    assert isinstance(residual_block, ResidualBlock)
+
+    with subtests.test("residual"):
+        assert isinstance(residual_block.residual, nn.Sequential)
+        assert len(residual_block.residual) == 4
+
+    with subtests.test("forward size"):
+        output_image = residual_block(input_image)
+        assert output_image.size() == input_image.size()
