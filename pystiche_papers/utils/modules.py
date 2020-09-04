41bb77324547c0619a202a657d1040a37cfd81dc
jbueltemeier <jubue@gmx.net>
2020-09-04 09:15:25 +0200

    Add PaddedConv2D

--- pystiche_papers/utils/modules.py
+++ pystiche_papers/utils/modules.py
@@ -1,7 +1,9 @@
-from typing import Any, Dict, Optional, Union, cast
+from typing import Any, Dict, List, Optional, Tuple, Union, cast
 
 import torch
 from torch import nn
+
+from ..utils import get_padding
 
 __all__ = [
     "Identity",
@@ -41,3 +43,51 @@ class SequentialWithOutChannels(nn.Sequential):
         self.out_channels = cast(Dict[str, nn.Module], self._modules)[
             out_channel_name
         ].out_channels
+
+
+class PaddedConv2D(nn.Conv2d):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[Tuple[int, int], int],
+        padding: str = "valid",
+        **kwargs: Any,
+    ) -> None:
+        self.use_correction_padding = False
+        if self.is_any_even(kernel_size) and padding == "same":
+            self.use_correction_padding = True
+            self.correction_padding = self.get_padding_correction(kernel_size)
+
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            padding=get_padding(padding, kernel_size),
+            **kwargs,
+        )
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        if self.use_correction_padding:
+            input = nn.functional.pad(input, self.correction_padding)
+        return cast(torch.Tensor, self._conv_forward(input, self.weight))
+
+    def is_any_even(self, inputs: Union[Tuple[int, int], int]) -> bool:
+        if isinstance(inputs, tuple):
+            return any(input % 2 == 0 for input in inputs)
+        return inputs % 2 == 0
+
+    def get_padding_correction(
+        self, kernel_size: Union[Tuple[int, int], int]
+    ) -> List[int]:
+        if isinstance(kernel_size, tuple):
+            return list(
+                sum(
+                    tuple(
+                        (0, 1) if self.is_any_even(size) else (0, 0)
+                        for size in kernel_size
+                    ),
+                    (),
+                )
+            )
+        return [0, 1, 0, 1]
