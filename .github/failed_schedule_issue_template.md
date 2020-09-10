1b21eb1ea6cd9984deb851a2bb808bcdad42dfed
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-09-10 15:50:55 +0200

    proof of concept

---
+++ .github/failed_schedule_issue_template.md
@@ -0,0 +1,12 @@
+---
+title: Scheduled workflow {{ env.WORKFLOW }}/{{ env.JOB }} failed
+assignees: pmeier
+labels: bug
+---
+
+Oh no, something went wrong in the scheduled workflow {{ env.WORKFLOW }}/{{ env.JOB }}. 
+Please look into it:
+
+https://github.com/{{ env.REPO}}/actions/runs/{{ env.ID }}
+
+Feel free to close this if this was just a one-off error.
---
+++ tests/download/test_gatys_ecker_bethge_2016.py
@@ -0,0 +1,13 @@
+import pytest
+
+import pystiche_papers.gatys_ecker_bethge_2016 as paper
+
+from tests.asserts import assert_image_downloads_correctly
+
+
+@pytest.mark.large_download
+@pytest.mark.slow
+def test_images(subtests):
+    for name, image in paper.images():
+        with subtests.test(name=name):
+            assert_image_downloads_correctly(image)
