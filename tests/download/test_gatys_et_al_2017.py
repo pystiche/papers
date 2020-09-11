113a746ec2780aeb97727277d76e894684220210
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-09-11 09:10:31 +0200

    reenable smoke tests for the papers

--- tests/download/test_gatys_et_al_2017.py
+++ tests/download/test_gatys_et_al_2017.py
@@ -2,9 +2,17 @@ import pytest
 
 import pystiche_papers.gatys_et_al_2017 as paper
 
-from .asserts import assert_image_downloads_correctly
+from .asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
 
+@pytest.mark.slow
+def test_images_smoke(subtests):
+    for name, image in paper.images():
+        with subtests.test(name=name):
+            assert_image_is_downloadable(image)
+
+
+@pytest.mark.large_download
 @pytest.mark.slow
 def test_images(subtests):
     for name, image in paper.images():
--- tests/download/test_ulyanov_et_al_2016.py
+++ tests/download/test_ulyanov_et_al_2016.py
@@ -2,9 +2,17 @@ import pytest
 
 import pystiche_papers.ulyanov_et_al_2016 as paper
 
-from .asserts import assert_image_downloads_correctly
+from .asserts import assert_image_downloads_correctly, assert_image_is_downloadable
 
 
+@pytest.mark.slow
+def test_images_smoke(subtests):
+    for name, image in paper.images():
+        with subtests.test(name=name):
+            assert_image_is_downloadable(image)
+
+
+@pytest.mark.large_download
 @pytest.mark.slow
 def test_images(subtests):
     for name, image in paper.images():
