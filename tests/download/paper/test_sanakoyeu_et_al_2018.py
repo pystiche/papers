7df1a206b35290af15e2afc9be1b3f97c6be7eb3
Philip Meier <github.pmeier@posteo.de>
2020-09-11 12:20:32 +0200

    move download tests for sanakoyeu_et_al_2018 (#191)

---
+++ tests/download/paper/test_sanakoyeu_et_al_2018.py
@@ -0,0 +1,13 @@
+import pytest
+
+import pystiche_papers.sanakoyeu_et_al_2018 as paper
+
+from ..asserts import assert_is_downloadable
+
+
+@pytest.mark.slow
+def test_WikiArt_downloadable(subtests):
+    base = paper.WikiArt.BASE_URL
+    styles = paper.WikiArt.STYLES
+    for style in styles:
+        assert_is_downloadable(f"{base}{style}.tar.gz")
