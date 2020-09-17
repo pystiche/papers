907d93e6ec988c6ad4e201fdf38fb3014786fe4a
Philip Meier <github.pmeier@posteo.de>
2020-09-17 09:27:04 +0200

    generalize URL selection from csv (#197)
    * generalize url selection from csv
    
    * use generalization for johnson_alahi_li_2016
    
    * add missing test
    
    * add error messages

--- tests/download/hub/test_johnson_alahi_li_2016.py
+++ tests/download/hub/test_johnson_alahi_li_2016.py
@@ -6,14 +6,12 @@ import pytest
 from torch import hub
 
 import pystiche_papers
+from pystiche_papers.utils import str_to_bool
 
 
 @pytest.mark.large_download
 @pytest.mark.slow
 def test_hub_johnson_alahi_li_2016_transformer(subtests, github):
-    def str_to_bool(string: str) -> bool:
-        return string.lower() == "true"
-
     def configs():
         file = path.join(
             pystiche_papers.__path__[0], "johnson_alahi_li_2016", "model_urls.csv"
