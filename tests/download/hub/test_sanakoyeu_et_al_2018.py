a2fa999883fb4526af44a9646dae14e9f3dd84a8
Philip Meier, Julian Bültemeier <github.pmeier@posteo.de>
2020-09-17 10:19:51 +0200

    enable transformer download via torch.hub

--- tests/download/hub/test_sanakoyeu_et_al_2018.py
+++ tests/download/hub/test_sanakoyeu_et_al_2018.py
@@ -11,18 +11,17 @@ from pystiche_papers.utils import str_to_bool
 
 @pytest.mark.large_download
 @pytest.mark.slow
-def test_hub_johnson_alahi_li_2016_transformer(subtests, github):
+def test_hub_sanakoyeu_et_al_2018_transformer(subtests, github):
     def configs():
         file = path.join(
-            pystiche_papers.__path__[0], "johnson_alahi_li_2016", "model_urls.csv"
+            pystiche_papers.__path__[0], "sanakoyeu_et_al_2018", "model_urls.csv"
         )
         with open(file, "r",) as fh:
             for row in csv.DictReader(fh):
                 row["impl_params"] = str_to_bool(row["impl_params"])
-                row["instance_norm"] = str_to_bool(row["instance_norm"])
                 del row["url"]
                 yield row
 
     for config in configs():
         with subtests.test(**config):
-            assert hub.load(github, "johnson_alahi_li_2016_transformer", **config)
+            assert hub.load(github, "sanakoyeu_et_al_2018_transformer", **config)
