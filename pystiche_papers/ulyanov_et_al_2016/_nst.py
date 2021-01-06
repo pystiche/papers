2cb7efd2b9cb4ec9de89568bca3b017a793ffbe0
jbueltemeier <49671359+jbueltemeier@users.noreply.github.com>
2021-01-06 08:06:32 +0100

    Update pystiche_papers/ulyanov_et_al_2016/_nst.py
    Co-authored-by: Philip Meier <github.pmeier@posteo.de>

--- pystiche_papers/ulyanov_et_al_2016/_nst.py
+++ pystiche_papers/ulyanov_et_al_2016/_nst.py
@@ -154,7 +154,7 @@ def stylization(
         transformer = _transformer(
             style=style, impl_params=impl_params, instance_norm=instance_norm,
         )
-    if instance_norm or not impl_params:
+    if not (impl_params and not instance_norm):
         # https://github.com/pmeier/texture_nets/blob/aad2cc6f8a998fedc77b64bdcfe1e2884aa0fb3e/test.lua#L32
         transformer = transformer.eval()
     transformer = transformer.to(device)
