4a507dfb534b326b6728391bb94d1d6faee3fd6a
Julian Bültemeier <jubue@gmx.net>
2022-10-20 11:11:22 +0200

    lint

--- rescience_paper/nst_images_script.py
+++ rescience_paper/nst_images_script.py
@@ -1,9 +1,8 @@
 import os
-from typing import Sequence,  Tuple, cast
+from typing import Sequence, Tuple, cast
 from argparse import Namespace
 from os import path
 
-import torch
 from torch import nn
 from pystiche import misc, optim, loss, enc, ops, image, data
 
@@ -26,7 +25,6 @@ def compute_layer_weights(
             f"attribute."
         )
 
-
     num_channels = []
     for layer in layers:
         if layer not in multi_layer_encoder:
@@ -34,7 +32,7 @@ def compute_layer_weights(
 
         num_channels.append(find_out_channels(multi_layer_encoder, layer))
 
-    return tuple(1.0 / n ** 2.0 for n in num_channels)
+    return tuple(1.0 / n**2.0 for n in num_channels)
 
 
 def nst_images():
@@ -64,40 +62,39 @@ def nst_images():
 
 
 def nst(
-        args,
-        content_image,
-        style_image,
-        output_name,
-        content_layer="relu4_2",
-        content_weight=1e0,
-        style_layers=("relu1_1", "relu2_1", "relu3_1", "relu4_1"),
-        style_weight=1e3,
-        starting_point="content",
-        num_steps=500,
+    args,
+    content_image,
+    style_image,
+    output_name,
+    content_layer="relu4_2",
+    content_weight=1e0,
+    style_layers=("relu1_1", "relu2_1", "relu3_1", "relu4_1"),
+    style_weight=1e3,
+    starting_point="content",
+    num_steps=500,
 ):
     multi_layer_encoder = enc.vgg19_multi_layer_encoder(
-        weights="caffe",
-        internal_preprocessing=True,
-        allow_inplace=True
+        weights="caffe", internal_preprocessing=True, allow_inplace=True
     )
 
     # content loss
     content_encoder = multi_layer_encoder.extract_encoder(content_layer)
     content_loss = ops.FeatureReconstructionOperator(
-        content_encoder,
-        score_weight=content_weight
+        content_encoder, score_weight=content_weight
     )
 
     # style loss
     layer_weights = compute_layer_weights(style_layers, multi_layer_encoder)
+
     def get_style_op(encoder, layer_weight):
         return ops.GramOperator(encoder, score_weight=layer_weight)
+
     style_loss = ops.MultiLayerEncodingOperator(
         multi_layer_encoder,
         style_layers,
         get_style_op,
         score_weight=style_weight,
-        layer_weights=layer_weights
+        layer_weights=layer_weights,
     )
 
     perceptual_loss = loss.PerceptualLoss(content_loss, style_loss).to(args.device)
@@ -108,7 +105,9 @@ def nst(
     perceptual_loss.set_style_image(style_image)
     input_image = misc.get_input_image(starting_point, content_image=content_image)
 
-    output_image = optim.image_optimization(input_image, perceptual_loss, num_steps=num_steps)
+    output_image = optim.image_optimization(
+        input_image, perceptual_loss, num_steps=num_steps
+    )
 
     output_file = path.join(args.image_results_dir, output_name)
     image.write_image(output_image, output_file)
@@ -117,12 +116,8 @@ def nst(
 def full_nst(args, content, style, size=500):
     images = nst_images()
     images.download(args.image_source_dir)
-    content_image = images[content].read(
-        size=size, device=args.device
-    )
-    style_image = images[style].read(
-        size=size, device=args.device
-    )
+    content_image = images[content].read(size=size, device=args.device)
+    style_image = images[style].read(size=size, device=args.device)
     name = f"nst_IST_paper_{content}__{style}__full.jpg"
     nst(args, content_image, style_image, name)
 
@@ -131,63 +126,76 @@ def layer_nst(args, content, style, size=500):
     images = nst_images()
     images.download(args.image_source_dir)
 
-    content_image = images[content].read(
-        size=size, device=args.device
-    )
-    style_image = images[style].read(
-        size=size, device=args.device
-    )
+    content_image = images[content].read(size=size, device=args.device)
+    style_image = images[style].read(size=size, device=args.device)
 
     # style loss
-    style_layers = ["relu1_1", "relu2_2", "relu3_1", "relu4_1",]
+    style_layers = [
+        "relu1_1",
+        "relu2_2",
+        "relu3_1",
+        "relu4_1",
+    ]
 
     for layer in style_layers:
         name = f"nst_IST_paper_{content}__{style}__{layer}.jpg"
-        nst(args, content_image, style_image, name, style_layers=[layer,], style_weight=1e3, starting_point="content")
+        nst(
+            args,
+            content_image,
+            style_image,
+            name,
+            style_layers=[layer],
+            style_weight=1e3,
+            starting_point="content",
+        )
 
 
 def init_nst(args, content, style, size=500):
     images = nst_images()
     images.download(args.image_source_dir)
 
-    content_image = images[content].read(
-        size=size, device=args.device
-    )
-    style_image = images[style].read(
-        size=size, device=args.device
-    )
+    content_image = images[content].read(size=size, device=args.device)
+    style_image = images[style].read(size=size, device=args.device)
 
     for starting_point in ["random", "content"]:
         name = f"nst_IST_paper_{content}__{style}__start_{starting_point}.jpg"
-        nst(args, content_image, style_image, name, style_weight=1e1, num_steps=1000, starting_point=starting_point)
+        nst(
+            args,
+            content_image,
+            style_image,
+            name,
+            style_weight=1e1,
+            num_steps=1000,
+            starting_point=starting_point,
+        )
 
 
 def iteration_nst(args, content, style, size=500):
     images = nst_images()
     images.download(args.image_source_dir)
 
-    content_image = images[content].read(
-        size=size, device=args.device
-    )
-    style_image = images[style].read(
-        size=size, device=args.device
-    )
+    content_image = images[content].read(size=size, device=args.device)
+    style_image = images[style].read(size=size, device=args.device)
 
     for num_steps in [100, 500, 1000]:
         name = f"nst_IST_paper_{content}__{style}__iteration_{num_steps}.jpg"
-        nst(args, content_image, style_image, name, num_steps=num_steps, starting_point="random", style_weight=1e1)
+        nst(
+            args,
+            content_image,
+            style_image,
+            name,
+            num_steps=num_steps,
+            starting_point="random",
+            style_weight=1e1,
+        )
 
 
 def image_size_nst(args, content, style):
     images = nst_images()
     images.download(args.image_source_dir)
     for image_size in [256, 512]:
-        content_image = images[content].read(
-            size=image_size, device=args.device
-        )
-        style_image = images[style].read(
-            size=image_size, device=args.device
-        )
+        content_image = images[content].read(size=image_size, device=args.device)
+        style_image = images[style].read(size=image_size, device=args.device)
 
         name = f"nst_IST_paper_{content}__{style}__size_{image_size}.jpg"
         nst(args, content_image, style_image, name)
@@ -197,30 +205,38 @@ def weights_nst(args, content, style, size=500):
     images = nst_images()
     images.download(args.image_source_dir)
     for weight in [1e0, 1e1, 1e2, 1e3]:
-        content_image = images[content].read(
-            size=size, device=args.device
-        )
-        style_image = images[style].read(
-            size=size, device=args.device
-        )
+        content_image = images[content].read(size=size, device=args.device)
+        style_image = images[style].read(size=size, device=args.device)
 
         name = f"nst_IST_paper_{content}__{style}__style_weight_{weight}.jpg"
-        nst(args, content_image, style_image, name, style_weight=weight, starting_point="random", num_steps=1000)
+        nst(
+            args,
+            content_image,
+            style_image,
+            name,
+            style_weight=weight,
+            starting_point="random",
+            num_steps=1000,
+        )
 
 
 def style_generation_nst(args, size=500):
     style = "starry_night"
     images = nst_images()
     images.download(args.image_source_dir)
-    content_image = images[style].read(
-        size=size, device=args.device
-    )
-    style_image = images[style].read(
-        size=size, device=args.device
-    )
+    content_image = images[style].read(size=size, device=args.device)
+    style_image = images[style].read(size=size, device=args.device)
 
     name = f"nst_IST_paper__{style}__generated.jpg"
-    nst(args, content_image, style_image, name, content_weight=0, starting_point="random", num_steps=1000)
+    nst(
+        args,
+        content_image,
+        style_image,
+        name,
+        content_weight=0,
+        starting_point="random",
+        num_steps=1000,
+    )
 
 
 def parse_input():
